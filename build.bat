d456b23f7906628136228ad41bfc3373b4d39c9e
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2014-12-09 20:47:25 -0600

    Better click vs commandline interactive awareness

--- build.bat
+++ build.bat
@@ -4,38 +4,71 @@ set interactive=1
 echo %cmdcmdline% | find /i "%~0" >nul
 if not errorlevel 1 set interactive=0
 
-if _%1_==__ (
-    echo usage: %~nx0 filename
-
-)
-
-
 
 REM ====================================================================
 REM   Build: Clean
 REM ====================================================================
 IF "%~1"=="clean" (
-    RMDIR build /S /Q
+    RMDIR build /S /Q && (
+      REM Do nothing
+    ) || (
+      if _%interactive%_==_0_ pause
+      if _%interactive%_==_0_ exit
+    )
     shift
+) ELSE IF _%interactive%_==_0_ (
+    RMDIR build /S /Q && (
+      REM Do nothing
+    ) || (
+      if _%interactive%_==_0_ pause
+      if _%interactive%_==_0_ exit
+    )
 )
+
+
 
 REM ====================================================================
 REM   Create: Build folder
 REM ====================================================================
-IF NOT EXIST "build" MKDIR build
+IF NOT EXIST "build" MKDIR build && (
+      REM Do nothing
+    ) || (
+      if _%interactive%_==_0_ pause
+      if _%interactive%_==_0_ exit
+    )
 
 REM ====================================================================
 REM   CMake: Clean any top-level cmake buid artifacts
 REM ====================================================================
-IF EXIST "cmake" RMDIR cmake /S /Q
-IF EXIST "CMakeFiles" RMDIR CMakeFiles /S /Q
-IF EXIST "CmakeCache.txt" DEL CmakeCache.txt
+IF EXIST "cmake" RMDIR cmake /S /Q && (
+      REM Do nothing
+    ) || (
+      if _%interactive%_==_0_ pause
+      if _%interactive%_==_0_ exit
+    )
+IF EXIST "CMakeFiles" RMDIR CMakeFiles /S /Q && (
+      REM Do nothing
+    ) || (
+      if _%interactive%_==_0_ pause
+      if _%interactive%_==_0_ exit
+    )
+IF EXIST "CmakeCache.txt" DEL CmakeCache.txt && (
+      REM Do nothing
+    ) || (
+      if _%interactive%_==_0_ pause
+      if _%interactive%_==_0_ exit
+    )
 
 
 REM ====================================================================
 REM   CMake: Begin cmake build process
 REM ====================================================================
-cd build
+cd build && (
+  REM Do nothing
+) || (
+  if _%interactive%_==_0_ pause
+  if _%interactive%_==_0_ exit
+)
 
 REM ====================================================================
 REM   CMake: Create build files
@@ -43,23 +76,26 @@ REM ====================================================================
 
 
 cmake ../ && (
-  echo Running cmake
+  REM Do nothing
 ) || (
   if _%interactive%_==_0_ pause
+  if _%interactive%_==_0_ exit
 )
 IF "%~1"=="rebuild" (
     make clean && (
       echo Running make clean
     ) || (
       if _%interactive%_==_0_ pause
+      if _%interactive%_==_0_ exit
     )
     shift
 )
 REM ====================================================================
 REM   CMake: Run Make
 REM ====================================================================
-make %*  && (
+make %* && (
   echo Running make %*
 ) || (
   if _%interactive%_==_0_ pause
+  if _%interactive%_==_0_ exit
 )

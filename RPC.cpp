238faef0c1fb2b325aaeca27396c486f4c10f237
czaloj <czaloj@gmail.com>
2014-12-10 08:35:01 -0500

    To be used as generic RPC

--- RPC.cpp
+++ RPC.cpp
@@ -1,17 +1,18 @@
 #include "stdafx.h"
-#include "GLRPC.h"
+#include "RPC.h"
 
-void vg::GLRPCManager::invoke(GLRPC* so, bool blockUntilFinished /*= true*/) {
+void vcore::RPCManager::invoke(RPC* so, bool blockUntilFinished /*= true*/) {
     m_queue.add(so, blockUntilFinished);
 }
-size_t vg::GLRPCManager::processRequests(size_t maxRequests /*= GLRPC_REQUESTS_PER_PROCESS*/) {
+size_t vcore::RPCManager::processRequests(size_t maxRequests /*= GLRPC_REQUESTS_PER_PROCESS*/) {
+    // Clamp number of requests
     if (maxRequests == 0) return 0;
-    if (maxRequests > GLRPC_REQUESTS_PER_PROCESS) maxRequests = GLRPC_REQUESTS_PER_PROCESS;
+    if (maxRequests > RPC_REQUESTS_PER_PROCESS) maxRequests = RPC_REQUESTS_PER_PROCESS;
 
     // Finish unfinished requests
     size_t n = maxRequests;
     while (m_unfinished.size() > 0 && n > 0) {
-        GLRPC* rpc = m_unfinished.front();
+        RPC* rpc = m_unfinished.front();
         m_unfinished.pop();
         
         rpc->data.f->invoke(this, rpc->data.userData);
@@ -21,13 +22,13 @@ size_t vg::GLRPCManager::processRequests(size_t maxRequests /*= GLRPC_REQUESTS_P
     if (n == 0) return maxRequests;
 
     // Obtain a bunch of requests
-    GLRPC* arr[GLRPC_REQUESTS_PER_PROCESS];
+    RPC* arr[RPC_REQUESTS_PER_PROCESS];
     size_t c = m_queue.getObjects(arr);
 
     // Handle all requests
     size_t i = 0;
     for (; i < c && n > 0; i++) {
-        GLRPCData& rpc = arr[i]->data;
+        RPCData& rpc = arr[i]->data;
 
         rpc.f->invoke(this, rpc.userData);
         m_queue.finish(arr[i]);
---
+++ RPC.h
@@ -0,0 +1,44 @@
+///
+/// RPC.h
+/// Vorb Engine
+///
+/// Created by Cristian Zaloj on 8 Dec 2014
+/// Copyright 2014 Regrowth Studios
+/// All Rights Reserved
+///
+/// Summary:
+/// 
+///
+
+#pragma once
+
+#ifndef RPC_h__
+#define RPC_h__
+
+#include "Events.hpp"
+#include "ThreadSync.h"
+
+namespace vorb {
+    namespace core {
+        struct RPCData {
+            IDelegate<void*>* f = nullptr;
+            void* userData = nullptr;
+        };
+
+        typedef ThreadSyncQueue<RPCData>::ThreadSyncObject RPC;
+
+#define RPC_REQUESTS_PER_PROCESS 8
+
+        class RPCManager {
+        public:
+            void invoke(RPC* so, bool blockUntilFinished = true);
+            size_t processRequests(size_t maxRequests = RPC_REQUESTS_PER_PROCESS);
+        private:
+            ThreadSyncQueue<RPCData> m_queue;
+            std::queue<RPC*> m_unfinished;
+        };
+    }
+}
+namespace vcore = vorb::core;
+
+#endif // RPC_h__

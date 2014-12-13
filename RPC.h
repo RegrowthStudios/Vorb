b6f9a6bcfd7acb3c3f60d7c8c80183b656391af3
czaloj <czaloj@gmail.com>
2014-12-12 20:42:56 -0500

    Document missed info

--- RPC.h
+++ RPC.h
@@ -7,7 +7,7 @@
 /// All Rights Reserved
 ///
 /// Summary:
-/// 
+/// Handles cross-thread function invocation and synchronization
 ///
 
 #pragma once
@@ -22,22 +22,31 @@
 
 namespace vorb {
     namespace core {
+        typedef IDelegate<void*> RPCFunction; ///< Supported RPC function prototype
+
         struct RPCData {
-            IDelegate<void*>* f = nullptr;
-            void* userData = nullptr;
+            RPCFunction* f = nullptr; ///< Function to invoke on separate thread
+            void* userData = nullptr; ///< Additional data passed on function invocation
         };
 
-        typedef ThreadSyncQueue<RPCData>::ThreadSyncObject RPC;
+        typedef ThreadSyncQueue<RPCData>::ThreadSyncObject RPC; ///< RPC call handle
 
 #define RPC_REQUESTS_PER_PROCESS 8
 
+        /// Manages cross-thread function calls
         class RPCManager {
         public:
+            /// Reports to target thread that a cross-thread call should be invoked upon it
+            /// @param so: RPC handle, can be used later for synchronization
+            /// @param blockUntilFinished: True if current thread should be blocked until RPC finishes
             void invoke(RPC* so, bool blockUntilFinished = true);
+            /// Invokes cross-thread calls, method should be called on the target thread
+            /// @param maxRequests: Maximum number of requests to process (clamped to RPC_REQUESTS_PER_PROCESS)
+            /// @return Number of requests that were processed
             size_t processRequests(size_t maxRequests = RPC_REQUESTS_PER_PROCESS);
         private:
-            ThreadSyncQueue<RPCData> m_queue;
-            std::queue<RPC*> m_unfinished;
+            ThreadSyncQueue<RPCData> m_queue; ///< Queue of idle requests
+            std::queue<RPC*> m_unfinished; ///< Queue of taken, but unfinished requests
         };
     }
 }

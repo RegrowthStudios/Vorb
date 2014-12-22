0f7105938f013e543ea0f8b854ec0fca289fa7d0
czaloj <czaloj@gmail.com>
2014-12-22 10:01:30 -0500

    Additional utilities

---
+++ ModelIO.h
@@ -0,0 +1,28 @@
+///
+/// ModelIO.h
+/// Vorb Engine
+///
+/// Created by Cristian Zaloj on 18 Dec 2014
+/// Copyright 2014 Regrowth Studios
+/// All Rights Reserved
+///
+/// Summary:
+/// Handles loading models into indexed-interleaved-vertex meshes
+///
+
+#pragma once
+
+#ifndef ModelIO_h__
+#define ModelIO_h__
+
+namespace vorb {
+    namespace io {
+        class ModelIO {
+        public:
+
+        };
+    }
+}
+namespace vio = vorb::io;
+
+#endif // ModelIO_h__

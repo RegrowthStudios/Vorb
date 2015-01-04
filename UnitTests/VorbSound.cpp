580438ef346d4babdadb3fcf048006c9ae78d14e
czaloj <czaloj@gmail.com>
2015-01-04 04:25:51 -0500

    Sound tests

--- UnitTests/VorbSound.cpp
+++ UnitTests/VorbSound.cpp
@@ -10,29 +10,126 @@
 #include <include/SoundResource.h>
 #include <include/Vorb.h>
 
-TEST(PlaySound) {
+TEST(Doppler) {
     if (vorb::init(vorb::InitParam::SOUND) != vorb::InitParam::SOUND) return false;
 
     puts("Loading Sound");
     vsound::Engine eng;
     eng.init();
-    vsound::Resource sound = eng.loadSound("data/Abyss.mp3");
-    vsound::Instance inst = eng.playInstance(sound);
+    vsound::Resource sound = eng.loadSound("data/c.ogg");
+    vsound::Instance inst = eng.createInstance(sound);
+    inst.setLooped(true);
+    inst.play();
 
     vsound::Listener l;
     puts("Playing Sound");
     f32 rot = 0.0f;
-    while (inst.isPlaying()) {
+    while (rot < 12) {
         l.velocity = l.position;
         l.position.x = cos(rot);
         l.position.z = 4.0f * sin(rot);
         l.velocity = l.position - l.velocity;
         l.velocity *= 20.0f;
-        rot += 0.1f;
+        rot += 0.2f;
 
         eng.update(l);
         Sleep(50);
     }
+    eng.freeSound(sound);
+    eng.dispose();
+
+    return vorb::dispose(vorb::InitParam::SOUND) == vorb::InitParam::SOUND;
+}
+
+TEST(SoundDefaults) {
+    if (vorb::init(vorb::InitParam::SOUND) != vorb::InitParam::SOUND) return false;
+
+    vsound::Engine eng;
+    eng.init();
+    vsound::Resource sound = eng.loadSound("data/Abyss.mp3");
+    vsound::Instance inst = eng.createInstance(sound);
+    eng.update(vsound::Listener());
+
+    f32v3 v3;
+    f32v2 v2;
+    printf("Is Playing: %s\n", inst.isPlaying() ? "True" : "False");
+    printf("Is Looped:  %s\n", inst.isLooped() ? "True" : "False");
+    v3 = inst.getPosition();
+    printf("Position:   (%f,%f,%f)\n", v3.x, v3.y, v3.z);
+    v3 = inst.getVelocity();
+    printf("Velocity:   (%f,%f,%f)\n", v3.x, v3.y, v3.z);
+    v2 = inst.getBoundsRadii();
+    printf("Radii:      [%f,%f]\n", v2.x, v2.y);
+    printf("Volume:     %f\n", inst.getVolume());
+    printf("Frequency:  %f\n", inst.getFrequency());
+    printf("Length:     %f seconds\n", inst.getLength());
+    
+    inst.setCursor(inst.getLength() * 0.5f);
+    inst.play();
+    Sleep(2000);
+    
+    eng.freeSound(sound);
+    eng.dispose();
+
+    return vorb::dispose(vorb::InitParam::SOUND) == vorb::InitParam::SOUND;
+}
+
+TEST(Frequency) {
+    if (vorb::init(vorb::InitParam::SOUND) != vorb::InitParam::SOUND) return false;
+
+    vsound::Engine eng;
+    eng.init();
+    vsound::Resource sound = eng.loadSound("data/c.ogg");
+    vsound::Instance inst = eng.createInstance(sound);
+    inst.setLooped(true);
+    inst.play();
+    vsound::Listener l;
+
+    f32 initial = inst.getFrequency();
+
+    inst.setFrequency(initial * 1.0f);
+    eng.update(l);
+    Sleep(1000);
+
+    inst.setFrequency(initial * 2.0f);
+    eng.update(l);
+    Sleep(1000);
+
+    inst.setFrequency(initial * 0.5f);
+    eng.update(l);
+    Sleep(1000);
+
+    eng.freeSound(sound);
+    eng.dispose();
+
+    return vorb::dispose(vorb::InitParam::SOUND) == vorb::InitParam::SOUND;
+}
+
+TEST(Volume) {
+    if (vorb::init(vorb::InitParam::SOUND) != vorb::InitParam::SOUND) return false;
+
+    vsound::Engine eng;
+    eng.init();
+    vsound::Resource sound = eng.loadSound("data/c.ogg");
+    vsound::Instance inst = eng.createInstance(sound);
+    inst.setLooped(true);
+    inst.play();
+    vsound::Listener l;
+
+    f32 initial = inst.getVolume();
+
+    inst.setVolume(initial * 1.0f);
+    eng.update(l);
+    Sleep(1000);
+
+    inst.setVolume(initial * 2.0f);
+    eng.update(l);
+    Sleep(1000);
+
+    inst.setVolume(initial * 0.5f);
+    eng.update(l);
+    Sleep(1000);
+
     eng.freeSound(sound);
     eng.dispose();
 

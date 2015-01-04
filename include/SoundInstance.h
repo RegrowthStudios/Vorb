754a288dbe633c8decc8214524ec4b5ad8d214b8
czaloj <czaloj@gmail.com>
2015-01-04 04:25:44 -0500

    Sound Instance effects

--- include/SoundInstance.h
+++ include/SoundInstance.h
@@ -28,9 +28,26 @@ namespace vorb {
 
             /// Play the sound if it's not already playing
             void play();
+            /// Pause the sound it it's playing
+            void pause();
+            void setLooped(const bool& loop);
+            void setCursor(const f32& seconds);
+            void setPosition(const f32v3& pos);
+            void setVelocity(const f32v3& vel);
+            void setBoundsRadii(const f32& min, const f32& max);
+            void setVolume(const f32& v);
+            void setFrequency(const f32& freq);
 
             /// @return True if this is playing
             bool isPlaying() const;
+            /// @return True if this is a looping sound
+            bool isLooped() const;
+            f32v3 getPosition() const;
+            f32v3 getVelocity() const;
+            f32v2 getBoundsRadii() const;
+            f32 getVolume() const;
+            f32 getFrequency() const;
+            f32 getLength() const;
         private:
             /// Create sound instance with reference to an engine
             /// @param engine: Divine creator of the cosmos
@@ -38,6 +55,8 @@ namespace vorb {
 
             Engine* m_engine = nullptr; ///< Handle to this instance's creator
             impl::InstanceData* m_data = nullptr; ///< Implementation-defined instance data
+
+            // TODO: Is it possible to cache results?
         };
     }
 }

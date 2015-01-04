303dcbc07811f44a60da40124640262ec4482d7d
czaloj <czaloj@gmail.com>
2015-01-04 02:41:46 -0500

    Test sound
    with added Doppler effect

--- UnitTests/VorbSound.cpp
+++ UnitTests/VorbSound.cpp
@@ -4,8 +4,6 @@
 #undef UNIT_TEST_BATCH
 #define UNIT_TEST_BATCH Vorb_Sound_
 
-#include <fmod/fmod.hpp>
-
 #include <include/SoundEngine.h>
 #include <include/SoundInstance.h>
 #include <include/SoundListener.h>
@@ -15,18 +13,28 @@
 TEST(PlaySound) {
     if (vorb::init(vorb::InitParam::SOUND) != vorb::InitParam::SOUND) return false;
 
+    puts("Loading Sound");
     vsound::Engine eng;
+    eng.init();
     vsound::Resource sound = eng.loadSound("data/Abyss.mp3");
-    vsound::Instance inst = eng.createSound(sound);
-    inst.play();
+    vsound::Instance inst = eng.playInstance(sound);
 
     vsound::Listener l;
-    l.position.z = 100;
+    puts("Playing Sound");
+    f32 rot = 0.0f;
     while (inst.isPlaying()) {
+        l.velocity = l.position;
+        l.position.x = cos(rot);
+        l.position.z = 4.0f * sin(rot);
+        l.velocity = l.position - l.velocity;
+        l.velocity *= 20.0f;
+        rot += 0.1f;
+
         eng.update(l);
-        Sleep(500);
+        Sleep(50);
     }
     eng.freeSound(sound);
+    eng.dispose();
 
     return vorb::dispose(vorb::InitParam::SOUND) == vorb::InitParam::SOUND;
 }

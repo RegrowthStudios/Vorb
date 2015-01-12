dd14ff00f470ba7106e0c5840fca26e0b53be84d
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-01-12 15:15:09 -0600

    Fixed a few bugs

--- cmake_files/vorb.cmake
+++ cmake_files/vorb.cmake
@@ -1,6 +1,6 @@
-include(utils.cmake)
-include(deps.cmake)
-include(components.cmake)
+include(cmake_files/utils.cmake)
+include(cmake_files/deps.cmake)
+include(cmake_files/components.cmake)
 
 verify_folder_structure()
 initialize_for_platform()

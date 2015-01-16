22543e4a66e101120929d8228f505f9f68ab0548
czaloj <czaloj@gmail.com>
2015-01-16 11:11:33 -0500

    Special scoped time sampling

--- include/Timing.h
+++ include/Timing.h
@@ -17,6 +17,8 @@
 
 #include <chrono>
 
+#include "Constants.h"
+
 class PreciseTimer {
 public:
     void start();
@@ -98,5 +100,131 @@ public:
 private:
     f32 m_maxFPS = DEFAULT_MAX_FPS;
 };
+
+namespace vorb {
+    template<typename T = ui64>
+    class ScopedSampler {
+    public:
+        ScopedSampler(T& context) :
+            m_context(context) {
+            m_time = std::chrono::high_resolution_clock::now();
+        }
+        ScopedSampler(T* context) : ScopedSampler(*context) {
+            // Empty
+        }
+        ~ScopedSampler() {
+            auto difference = std::chrono::high_resolution_clock::now() - m_time;
+            m_context += std::chrono::duration_cast<std::chrono::microseconds>(difference).count();
+        }
+    private:
+        T& m_context;
+        std::chrono::system_clock::time_point m_time {};
+    };
+
+    class AccumulationSamplerContext {
+    public:
+        AccumulationSamplerContext& operator+=(const ui64& elapsedTicks) {
+            m_ticks += elapsedTicks;
+            return *this;
+        }
+
+        const ui64& getAccumulatedTicks() const {
+            return m_ticks;
+        }
+        const f64 getAccumulatedMilliSeconds() const {
+            return (f64)m_ticks / MICROSECONDS_PER_MILLISECOND_F64;
+        }
+        const f64 getAccumulatedSeconds() const {
+            return (f64)m_ticks / MICROSECONDS_PER_SECOND_F64;
+        }
+    protected:
+        ui64 m_ticks = 0;
+    };
+    typedef ScopedSampler<AccumulationSamplerContext> ScopedAccumulationSampler;
+
+    class AveragedSamplerContext {
+    public:
+        AveragedSamplerContext& operator+=(const ui64& elapsedTicks) {
+            m_ticks += elapsedTicks;
+            m_entries++;
+            return *this;
+        }
+
+        const ui64& getAccumulatedTicks() const {
+            return m_ticks;
+        }
+        const f64 getAccumulatedMilliSeconds() const {
+            return (f64)m_ticks / MICROSECONDS_PER_MILLISECOND_F64;
+        }
+        const f64 getAccumulatedSeconds() const {
+            return (f64)m_ticks / MICROSECONDS_PER_SECOND_F64;
+        }
+
+        const ui64& getEntryCount() const {
+            return m_ticks;
+        }
+        const ui64 getAverageTicks() const {
+            ui64 rounded = m_ticks + (m_entries >> 1);
+            return rounded / m_entries;
+        }
+        const f64 getAverageMilliseconds() const {
+            return getAccumulatedMilliSeconds() / (f64)m_entries;
+        }
+        const f64 getAverageSeconds() const {
+            return getAccumulatedSeconds() / (f64)m_entries;
+        }
+    protected:
+        ui64 m_ticks = 0;
+        ui64 m_entries = 0;
+    };
+    typedef ScopedSampler<AveragedSamplerContext> ScopedAveragedSampler;
+
+    class DetailedSamplerContext {
+    public:
+        DetailedSamplerContext& operator+=(const ui64& elapsedTicks) {
+            m_ticks += elapsedTicks;
+            m_entries++;
+            if (elapsedTicks < m_bounds.x) m_bounds.x = elapsedTicks;
+            if (elapsedTicks > m_bounds.y) m_bounds.y = elapsedTicks;
+            return *this;
+        }
+
+        const ui64& getAccumulatedTicks() const {
+            return m_ticks;
+        }
+        const f64 getAccumulatedMilliSeconds() const {
+            return (f64)m_ticks / MICROSECONDS_PER_MILLISECOND_F64;
+        }
+        const f64 getAccumulatedSeconds() const {
+            return (f64)m_ticks / MICROSECONDS_PER_SECOND_F64;
+        }
+
+        const ui64& getEntryCount() const {
+            return m_ticks;
+        }
+        const ui64 getAverageTicks() const {
+            ui64 rounded = m_ticks + (m_entries >> 1);
+            return rounded / m_entries;
+        }
+        const f64 getAverageMilliseconds() const {
+            return getAccumulatedMilliSeconds() / (f64)m_entries;
+        }
+        const f64 getAverageSeconds() const {
+            return getAccumulatedSeconds() / (f64)m_entries;
+        }
+
+        const ui64& getMinTicks() const {
+            return m_bounds.x;
+        }
+        const ui64& getMaxTicks() const {
+            return m_bounds.y;
+        }
+    protected:
+        ui64 m_ticks = 0;
+        ui64 m_entries = 0;
+        ui64v2 m_bounds { 0ui64 - 1 , 0 };
+    };
+    typedef ScopedSampler<DetailedSamplerContext> ScopedDetailedSampler;
+}
 
 #endif // Timing_h__

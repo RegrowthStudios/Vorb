dbef884b3ac72fa8eaea48a0956658d09ebd174a
czaloj <czaloj@gmail.com>
2015-01-26 14:35:30 -0500

    Move bullet as a separate-built dep

--- DepsBuildCopy.bat
+++ DepsBuildCopy.bat
@@ -1,13 +1,55 @@
 @ECHO OFF
-COPY  /Y "deps\src\bin\Win32\Release\YAML\libyaml-cppmd.lib " "deps\lib\Win32\libyaml-cppmd.lib"
-COPY  /Y "deps\src\bin\Win32\Debug\YAML\libyaml-cppmdd.lib "   "deps\lib\Win32\libyaml-cppmdd.lib"
-COPY  /Y "deps\src\bin\x64\Release\YAML\libyaml-cppmd.lib "   "deps\lib\x64\libyaml-cppmd.lib"
-COPY  /Y "deps\src\bin\x64\Debug\YAML\libyaml-cppmdd.lib "     "deps\lib\x64\libyaml-cppmdd.lib"
-COPY  /Y "deps\src\bin\Win32\Release\OOLua\OOLua.lib " "deps\lib\Win32\OOLua.lib"
-COPY  /Y "deps\src\bin\Win32\Debug\OOLua\OOLua-d.lib "   "deps\lib\Win32\OOLua-d.lib"
-COPY  /Y "deps\src\bin\x64\Release\OOLua\OOLua.lib "   "deps\lib\x64\OOLua.lib"
-COPY  /Y "deps\src\bin\x64\Debug\OOLua\OOLua-d.lib "     "deps\lib\x64\OOLua-d.lib"
-COPY  /Y "deps\src\bin\Win32\Release\ZLIB\ZLIB.lib " "deps\lib\Win32\ZLIB.lib"
-COPY  /Y "deps\src\bin\Win32\Debug\ZLIB\ZLIB-d.lib "   "deps\lib\Win32\ZLIB-d.lib"
-COPY  /Y "deps\src\bin\x64\Release\ZLIB\ZLIB.lib "   "deps\lib\x64\ZLIB.lib"
-COPY  /Y "deps\src\bin\x64\Debug\ZLIB\ZLIB-d.lib "     "deps\lib\x64\ZLIB-d.lib"
+
+SET LIBNAME=libyaml-cppmd.lib
+SET LIBNAME_DEBUG=libyaml-cppmdd.lib
+SET LIBFOLDER=YAML
+ECHO Copying YAML
+COPY  /Y "deps\src\bin\Win32\Release\%LIBFOLDER%\%LIBNAME% " "deps\lib\Win32\%LIBNAME%"
+COPY  /Y "deps\src\bin\Win32\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "   "deps\lib\Win32\%LIBNAME_DEBUG%"
+COPY  /Y "deps\src\bin\x64\Release\%LIBFOLDER%\%LIBNAME% "   "deps\lib\x64\%LIBNAME%"
+COPY  /Y "deps\src\bin\x64\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "     "deps\lib\x64\%LIBNAME_DEBUG%"
+
+SET LIBNAME=OOLua.lib
+SET LIBNAME_DEBUG=OOLua-d.lib
+SET LIBFOLDER=OOLua
+ECHO Copying Lua
+COPY  /Y "deps\src\bin\Win32\Release\%LIBFOLDER%\%LIBNAME% " "deps\lib\Win32\%LIBNAME%"
+COPY  /Y "deps\src\bin\Win32\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "   "deps\lib\Win32\%LIBNAME_DEBUG%"
+COPY  /Y "deps\src\bin\x64\Release\%LIBFOLDER%\%LIBNAME% "   "deps\lib\x64\%LIBNAME%"
+COPY  /Y "deps\src\bin\x64\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "     "deps\lib\x64\%LIBNAME_DEBUG%"
+
+SET LIBNAME=ZLIB.lib
+SET LIBNAME_DEBUG=ZLIB-d.lib
+SET LIBFOLDER=ZLIB
+ECHO Copying ZLIB
+COPY  /Y "deps\src\bin\Win32\Release\%LIBFOLDER%\%LIBNAME% " "deps\lib\Win32\%LIBNAME%"
+COPY  /Y "deps\src\bin\Win32\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "   "deps\lib\Win32\%LIBNAME_DEBUG%"
+COPY  /Y "deps\src\bin\x64\Release\%LIBFOLDER%\%LIBNAME% "   "deps\lib\x64\%LIBNAME%"
+COPY  /Y "deps\src\bin\x64\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "     "deps\lib\x64\%LIBNAME_DEBUG%"
+
+SET LIBNAME=LinearMath.lib
+SET LIBNAME_DEBUG=LinearMath_Debug.lib
+SET LIBFOLDER=Bullet
+ECHO Copying Bullet Math
+COPY  /Y "deps\src\bin\Win32\Release\%LIBFOLDER%\%LIBNAME% " "deps\lib\Win32\%LIBNAME%"
+COPY  /Y "deps\src\bin\Win32\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "   "deps\lib\Win32\%LIBNAME_DEBUG%"
+COPY  /Y "deps\src\bin\x64\Release\%LIBFOLDER%\%LIBNAME% "   "deps\lib\x64\%LIBNAME%"
+COPY  /Y "deps\src\bin\x64\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "     "deps\lib\x64\%LIBNAME_DEBUG%"
+
+SET LIBNAME=BulletCollision.lib
+SET LIBNAME_DEBUG=BulletCollision_Debug.lib
+SET LIBFOLDER=Bullet
+ECHO Copying Bullet Collision
+COPY  /Y "deps\src\bin\Win32\Release\%LIBFOLDER%\%LIBNAME% " "deps\lib\Win32\%LIBNAME%"
+COPY  /Y "deps\src\bin\Win32\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "   "deps\lib\Win32\%LIBNAME_DEBUG%"
+COPY  /Y "deps\src\bin\x64\Release\%LIBFOLDER%\%LIBNAME% "   "deps\lib\x64\%LIBNAME%"
+COPY  /Y "deps\src\bin\x64\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "     "deps\lib\x64\%LIBNAME_DEBUG%"
+
+SET LIBNAME=BulletDynamics.lib
+SET LIBNAME_DEBUG=BulletDynamics_Debug.lib
+SET LIBFOLDER=Bullet
+ECHO Copying Bullet Dynamics
+COPY  /Y "deps\src\bin\Win32\Release\%LIBFOLDER%\%LIBNAME% " "deps\lib\Win32\%LIBNAME%"
+COPY  /Y "deps\src\bin\Win32\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "   "deps\lib\Win32\%LIBNAME_DEBUG%"
+COPY  /Y "deps\src\bin\x64\Release\%LIBFOLDER%\%LIBNAME% "   "deps\lib\x64\%LIBNAME%"
+COPY  /Y "deps\src\bin\x64\Debug\%LIBFOLDER%\%LIBNAME_DEBUG% "     "deps\lib\x64\%LIBNAME_DEBUG%"

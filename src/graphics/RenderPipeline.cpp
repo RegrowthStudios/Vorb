dafb9aa50fcecccd3963ad588834c9b0d7b90723
barnold1953 <brb555@utk.edu>
2015-03-29 14:51:34 -0400

    Refactored and enhanced IRenderPipeline and IRenderStage

---
+++ src/graphics/RenderPipeline.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "graphics/RenderPipeline.h"
+#include "graphics/IRenderStage.h"
+
+vg::RenderPipeline::RenderPipeline() {
+    // Empty
+}
+
+vg::RenderPipeline::~RenderPipeline() {
+    // Empty
+}
+
+void vorb::graphics::RenderPipeline::setCamera(Camera* camera) {
+    m_camera = camera;
+    for (auto& stage : m_stages) {
+        stage->setCamera(camera);
+    }
+}
+
+void vorb::graphics::RenderPipeline::addStage(StagePtr stage) {
+    m_stages.push_back(stage);
+}
+
+void vorb::graphics::RenderPipeline::render() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    for (auto& stage : m_stages) {
+        if (stage->isVisible()) stage->render();
+    }
+}
+
+void vorb::graphics::RenderPipeline::destroy() {
+    for (auto& stage : m_stages) {
+        stage.reset();
+    }
+    std::vector <StagePtr>().swap(m_stages);
+}
+
+void vorb::graphics::RenderPipeline::reloadShaders() {
+    for (auto& stage : m_stages) {
+        stage->reloadShader();
+    }
+}

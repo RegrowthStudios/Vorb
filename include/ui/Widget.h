2cf7ac1bee6bc9a3a511e71cf904376b2a049004
barnold1953 <brb555@utk.edu>
2015-05-02 16:01:20 -0400

    Renamed control to widget and fixed dock style

--- include/ui/Widget.h
+++ include/ui/Widget.h
@@ -1,5 +1,5 @@
 //
-// Control.h
+// Widget.h
 // Vorb Engine
 //
 // Created by Benjamin Arnold on 27 Apr 2015
@@ -7,17 +7,17 @@
 // All Rights Reserved
 //
 
-/*! \file Control.h
+/*! \file Widget.h
 * @brief 
-* Base class for all Controls.
+* Base class for all Widgets.
 *
 */
 
 #pragma once
 
-#ifndef Vorb_Control_h__
+#ifndef Vorb_Widget_h__
 //! @cond DOXY_SHOW_HEADER_GUARDS
-#define Vorb_Control_h__
+#define Vorb_Widget_h__
 //! @endcond
 
 #ifndef VORB_USING_PCH
@@ -28,8 +28,7 @@
 #include "../Events.hpp"
 #include "../VorbPreDecl.inl"
 
-DECL_VG(class SpriteFont;
-        class SpriteBatch)
+DECL_VG(class SpriteFont)
 
 namespace vorb {
     namespace ui {
@@ -52,23 +51,20 @@ namespace vorb {
             bool top : 1; ///< If true, anchored to the top of parent
         };
         //! Bitfield of dock flags
-        struct DockStyle {
-            bool left : 1; ///< If true, docked to the left of parent
-            bool right : 1; ///< If true, docked to the right of parent
-            bool bottom : 1; ///< If true, docked to the bottom of parent
-            bool top : 1; ///< If true, anchored to the top of parent
+        enum class DockStyle {
+            LEFT, RIGHT, BOTTOM, TOP, FILL
         };
 
-        class Control {
+        class Widget {
         public:
             /*! @brief Default constructor. */
-            Control();
+            Widget();
             /*! @brief Constructor that sets name, position, and dimensions.
              *
              * @param name: Name of the control.
              * @param destRect: Rectangle defining the position and dimensions as the tuple <x,y,w,h>.
              */
-            Control(const nString& name, const ui32v4& destRect = ui32v4(0));
+            Widget(const nString& name, const ui32v4& destRect = ui32v4(0));
             /*! @brief Constructor that sets parent control, name, position, and dimensions.
              *
              * The control will be made a child of parent.
@@ -77,23 +73,14 @@ namespace vorb {
              * @param name: Name of the control.
              * @param destRect: Rectangle defining the position and dimensions as the tuple <x,y,w,h>.
              */
-            Control(Control* parent, const nString& name, const ui32v4& destRect = ui32v4(0));
+            Widget(Widget* parent, const nString& name, const ui32v4& destRect = ui32v4(0));
             /*! @brief Destructor that unhooks events */
-            virtual ~Control();
-
-            /*! @brief Initializes the rendering
-            *
-            * @param sb: The spritebatch to use.
-            * @param font: The spritefont to use.
-            * @return true on success.
-            */
-            virtual bool initRendering(vorb::graphics::SpriteBatch* sb,
-                                       vorb::graphics::SpriteFont* font);
+            virtual ~Widget();
 
             /*! @brief Draws the control.
-            *
-            * Gets called in the destructor.
-            */
+             *
+             * Gets called in the destructor.
+             */
             virtual void draw();
 
             /*! @brief Releases all resources used by the Control.
@@ -103,12 +90,11 @@ namespace vorb {
             virtual void dispose();
 
             /*! @brief Adds a child to the control
-            *
-            * @param child: The control to add
-            * @param name: Name of the control.
-            * @param destRect: Rectangle defining the position and dimensions as the tuple <x,y,w,h>.
-            */
-            virtual bool addChild(Control* child);
+             *
+             * @param child: The control to add
+             * @return true on success.
+             */
+            virtual bool addChild(Widget* child);
             
             /************************************************************************/
             /* Getters                                                              */
@@ -117,7 +103,7 @@ namespace vorb {
             virtual bool getFixedWidth() const { return m_style.fixedWidth; }
             virtual bool getSelectable() const { return m_style.selectable; }
             virtual const ControlStyle& getStyle() const { return m_style; }
-            virtual std::vector<Control*>& getControls() { return m_controls; }
+            virtual const std::vector<Widget*>& getControls() const { return m_controls; }
             virtual const ui32v4& getDestRect() const { return m_destRect; }
             virtual const ui32& getX() const { return m_destRect.x; }
             virtual const ui32& getY() const { return m_destRect.y; }
@@ -126,7 +112,6 @@ namespace vorb {
             virtual const AnchorStyle& getAnchor() const { return m_anchor; }
             virtual const DockStyle& getDock() const { return m_dock; }
             virtual const bool& isRenderInit() const { return m_isRenderInit; }
-            virtual const vorb::graphics::SpriteBatch* getSpriteBatch() const { return m_sb; }
             virtual const vorb::graphics::SpriteFont* getFont() const { return m_font; }
             
             /************************************************************************/
@@ -144,7 +129,6 @@ namespace vorb {
             virtual void setHeight(ui32 height) { m_destRect.w = height; }
             virtual void setAnchor(const AnchorStyle& anchor) { m_anchor = anchor; }
             virtual void setDock(const DockStyle& dock) { m_dock = dock; }
-            virtual void setSpriteBatch(vorb::graphics::SpriteBatch* sb);
             virtual void setFont(vorb::graphics::SpriteFont* font);
 
             /************************************************************************/
@@ -166,18 +150,17 @@ namespace vorb {
             virtual void onMouseLeave(Sender s, const MouseEvent& e);
             virtual void onMouseMove(Sender s, const MouseEvent& e);
 
-            vorb::graphics::SpriteFont* m_font = nullptr;
-            vorb::graphics::SpriteBatch* m_sb = nullptr;
-            AnchorStyle m_anchor; ///< The anchor data
-            ControlStyle m_style = {}; ///< The current style
-            DockStyle m_dock; ///< The dock data
-            std::vector<Control*> m_controls; ///< All child controls
-            ui32v4 m_destRect; ///< The position and dimensions
+            vorb::graphics::SpriteFont* m_font = nullptr; ///< Font for rendering.
+            AnchorStyle m_anchor; ///< The anchor data.
+            ControlStyle m_style = {}; ///< The current style.
+            DockStyle m_dock; ///< The dock type.
+            std::vector<Widget*> m_controls; ///< All child controls.
+            ui32v4 m_destRect; ///< The position and dimensions.
 
-            bool m_isRenderInit = false; ///< True when rendering is initialized
+            bool m_isRenderInit = false; ///< True when rendering is initialized.
         };
     }
 }
 namespace vui = vorb::ui;
 
-#endif // !Vorb_Control_h__
+#endif // !Vorb_Widget_h__

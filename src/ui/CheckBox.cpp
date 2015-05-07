e13c158991ca205a74f893a4b2956a50bfaf3568
barnold1953 <brb555@utk.edu>
2015-05-06 20:15:27 -0400

    Added checkbox outline

---
+++ src/ui/CheckBox.cpp
@@ -0,0 +1,122 @@
+#include "stdafx.h"
+#include "ui/CheckBox.h"
+
+vui::CheckBox::CheckBox() {
+
+}
+
+vui::CheckBox::CheckBox(const nString& name, const ui32v4& destRect /*= ui32v4(0)*/) {
+
+}
+
+vui::CheckBox::CheckBox(Widget* parent, const nString& name, const ui32v4& destRect /*= ui32v4(0)*/) {
+
+}
+
+vui::CheckBox::~CheckBox() {
+
+}
+
+void vui::CheckBox::addDrawables(UIRenderer* renderer) {
+
+}
+
+void vui::CheckBox::removeDrawables(UIRenderer* renderer) {
+
+}
+
+void vui::CheckBox::check() {
+
+}
+
+void vui::CheckBox::unCheck() {
+
+}
+
+void vui::CheckBox::toggleChecked() {
+
+}
+
+void vui::CheckBox::setDimensions(const f32v2& dimensions) {
+
+}
+
+void vui::CheckBox::setFont(vg::SpriteFont* font) {
+
+}
+
+void vui::CheckBox::setHeight(f32 height) {
+
+}
+
+void vui::CheckBox::setPosition(const f32v2& position) {
+
+}
+
+void vui::CheckBox::setCheckedTexture(VGTexture texture) {
+
+}
+
+void vui::CheckBox::setUncheckedTexture(VGTexture texture) {
+
+}
+
+void vui::CheckBox::setWidth(f32 width) {
+
+}
+
+void vui::CheckBox::setX(f32 x) {
+
+}
+
+void vui::CheckBox::setY(f32 y) {
+
+}
+
+void vui::CheckBox::setBackColor(const color4& color) {
+
+}
+
+void vui::CheckBox::setBackHoverColor(const color4& color) {
+
+}
+
+void vui::CheckBox::setText(const nString& text) {
+
+}
+
+void vui::CheckBox::setTextColor(const color4& color) {
+
+}
+
+void vui::CheckBox::setTextHoverColor(const color4& color) {
+
+}
+
+void vui::CheckBox::setTextAlign(vg::TextAlign textAlign) {
+
+}
+
+void vui::CheckBox::setTextScale(const f32v2& textScale) {
+
+}
+
+void vui::CheckBox::setChecked(bool checked) {
+
+}
+
+void vui::CheckBox::updateColor() {
+
+}
+
+void vui::CheckBox::updateTextPosition() {
+
+}
+
+void vui::CheckBox::refreshDrawables() {
+
+}
+
+void vui::CheckBox::onMouseMove(Sender s, const MouseMotionEvent& e) {
+
+}

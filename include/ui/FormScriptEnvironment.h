aef9670cf26ebe2052f078f8b088f457286c6108
barnold1953 <brb555@utk.edu>
2015-05-11 10:46:39 -0400

    Basic implementation of init with FormScriptEnvironment for buttons

---
+++ include/ui/FormScriptEnvironment.h
@@ -0,0 +1,51 @@
+//
+// FormScriptEnvironment.h
+// Vorb Engine
+//
+// Created by Benjamin Arnold on 11 May 2015
+// Copyright 2014 Regrowth Studios
+// All Rights Reserved
+//
+
+/*! \file FormScriptEnvironment.h
+* @brief 
+* Scripting environment for a UI form.
+*
+*/
+
+#pragma once
+
+#ifndef Vorb_FormScriptEnvironment_h__
+//! @cond DOXY_SHOW_HEADER_GUARDS
+#define Vorb_FormScriptEnvironment_h__
+//! @endcond
+
+#ifndef VORB_USING_PCH
+#include "types.h"
+#endif // !VORB_USING_PCH
+
+#include "../script/Environment.h"
+
+namespace vorb {
+    namespace ui {
+
+        // Forward Declarations
+        class Form;
+
+        class FormScriptEnvironment {
+        public:
+            FormScriptEnvironment();
+            virtual ~FormScriptEnvironment();
+            virtual void init(Form* form, const cString filePath);
+        protected:
+            virtual int makeButton(const nString& name, f32 x, f32 y, f32 width, f32 height);
+
+            Form* m_form = nullptr;
+            vscript::Environment m_env;
+            vscript::Function m_init;
+        };
+    }
+}
+namespace vui = vorb::ui;
+
+#endif // !Vorb_FormScriptEnvironment_h__
---
+++ src/ui/FormScriptEnvironment.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "ui/FormScriptEnvironment.h"
+#include "ui/IButton.h"
+#include "ui/Form.h"
+
+#define INIT_FUNCTION_NAME "init"
+
+vui::FormScriptEnvironment::FormScriptEnvironment() {
+    // Empty
+}
+
+vui::FormScriptEnvironment::~FormScriptEnvironment() {
+    // Empty
+}
+
+void vui::FormScriptEnvironment::init(Form* form, const cString filePath) {
+
+    { // Initialize callbacks
+        m_env.setNamespaces("Form");
+        m_env.addCRDelegate("makeButton", makeRDelegate(*this, &FormScriptEnvironment::makeButton));
+        m_env.setNamespaces();
+    }
+
+    // Load script
+    m_env.load(filePath);
+
+    { // Call init function
+        m_init = m_env[INIT_FUNCTION_NAME];
+        m_init();
+    }
+}
+
+int vui::FormScriptEnvironment::makeButton(const nString& name, f32 x, f32 y, f32 width, f32 height) {
+    vui::IButton* b = new vui::IButton(name, f32v4(x, y, width, height));
+    m_form->addWidget(b);
+}

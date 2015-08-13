21dd9774aa59fbb9f5e47ba0941210b64fd2dc36
czaloj <czaloj@gmail.com>
2015-08-13 15:03:58 -0700

    Test if blanked member function invocation works

---
+++ UnitTests/DelegateEvents.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "macros.h"
+
+#undef UNIT_TEST_BATCH
+#define UNIT_TEST_BATCH DE_
+
+#include "include/Events.hpp"
+
+class A {
+public:
+    bool getOppositeValue(bool v) {
+        return !v;
+    }
+};
+
+
+class BlankTemplate {
+    template<typename Ret, typename... Params>
+    Ret function(Params... args);
+};
+
+TEST(BlankFunctionAccess) {
+    A value = {};
+
+    void* obj = &value;
+    PointerCast<bool (A::*)(bool)> fObj = { &A::getOppositeValue };
+
+    BlankTemplate* blankedObject = (BlankTemplate*)obj;
+    PointerCast<bool (BlankTemplate::*)(bool)> blankedFunction = { fObj.ptr };
+    
+    return (blankedObject->*blankedFunction.value)(false);
+}

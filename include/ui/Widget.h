82c610b9e2a23df127b646e314056fe821503cce
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-06-28 03:49:45 +0100

    Beginning of modification to positioning and sizing.

--- include/ui/Widget.h
+++ include/ui/Widget.h
@@ -46,6 +46,27 @@ namespace vorb {
             CENTER
         };
 
+        enum class PositionType {
+            STATIC,
+            ABSOLUTE,
+            FIXED_TO_WINDOW,
+            FIXED_TO_FORM,
+            RELATIVE
+        };
+
+        enum class DimensionType {
+            PIXEL,
+            PERCENTAGE,
+            FORM_WIDTH_PERC,
+            FORM_HEIGHT_PERC,
+            FORM_MIN_PERC,
+            FORM_MAX_PERC,
+            WINDOW_WIDTH_PERC,
+            WINDOW_HEIGHT_PERC,
+            WINDOW_MIN_PERC,
+            WINDOW_MAX_PERC
+        };
+
         // Forward Declarations
         class UIRenderer;
 
@@ -100,33 +121,38 @@ namespace vorb {
             /* Getters                                                              */
             /************************************************************************/ 
             virtual const AnchorStyle& getAnchor() const { return m_anchor; }
-            virtual const DockStyle& getDock() const { return m_dock; }
             virtual const IWidgetContainer* getParent() const { return m_parent; }
+            virtual const PositionType& getPositionType() const { return m_positionType; }
             virtual const volatile bool& needsDrawableReload() const { return m_needsDrawableReload; }
             virtual const vorb::graphics::SpriteFont* getFont() const { return m_font; }
             virtual const UIRenderer* getRenderer() const { return m_renderer; }
-            virtual const f32v2& getMinSize() const { return m_minSize; }
-            virtual const f32v2& getMaxSize() const { return m_maxSize; }
-            virtual const f32v2& getPositionPercentage() const { return m_positionPercentage; }
-            virtual const f32v2& getDimensionsPercentage() const { return m_dimensionsPercentage; }
+            virtual const std::pair<f32v2, DimensionType>& getRawPosition() const { return m_rawPosition; }
+            virtual const std::pair<f32v2, DimensionType>& getRawDimensions() const { return m_rawDimensions; }
+            // TODO(Matthew): Add functionality to return in pixels specifically?
+            virtual const std::pair<f32v2, DimensionType>& getMinSize() const { return m_minSize; }
+            virtual const std::pair<f32v2, DimensionType>& getMaxSize() const { return m_maxSize; }
             virtual const WidgetAlign& getWidgetAlign() const { return m_align; }
 
             /************************************************************************/
             /* Setters                                                              */
             /************************************************************************/
             virtual void setAnchor(const AnchorStyle& anchor);
-            virtual void setDock(const DockStyle& dock);
             virtual void setFont(const vorb::graphics::SpriteFont* font) { m_font = font; }
             virtual void setNeedsDrawableReload(bool needsDrawableReload) { m_needsDrawableReload = needsDrawableReload; }
             virtual void setParent(IWidgetContainer* parent);
-            virtual void setPositionPercentage(const f32v2& positionPercentage) { m_positionPercentage = positionPercentage; updatePosition(); }
-            virtual void setDimensionsPercentage(const f32v2& dimensionsPercentage) { m_dimensionsPercentage = dimensionsPercentage; updateDimensions(); }
-            virtual void setXPercentage(f32 xPercentage) { m_positionPercentage.x = xPercentage; updatePosition(); }
-            virtual void setYPercentage(f32 yPercentage) { m_positionPercentage.y = yPercentage; updatePosition(); }
-            virtual void setMaxSize(const f32v2& maxSize) { m_maxSize = maxSize; updatePosition(); }
-            virtual void setMinSize(const f32v2& minSize) { m_minSize = minSize; updatePosition(); }
-            virtual void setWidthPercentage(f32 widthPercentage) { m_dimensionsPercentage.x = widthPercentage; updateDimensions(); }
-            virtual void setHeightPercentage(f32 heightPercentage) { m_dimensionsPercentage.y = heightPercentage; updateDimensions(); }
+            virtual void setPositionType(PositionType& positionType) { m_positionType = positionType; updatePosition(); }
+            virtual void setRawPosition(const std::pair<f32v2, DimensionType>& rawPosition) { m_rawPosition = rawPosition; updatePosition(); }
+            virtual void setRawPosition(const f32v2& rawPosition, DimensionType& dimensionType) { m_rawPosition.first = rawPosition; m_rawPosition.second = dimensionType; updatePosition(); }
+            virtual void setRawDimensions(const std::pair<f32v2, DimensionType>& rawDimensions) { m_rawDimensions = rawDimensions; updatePosition(); }
+            virtual void setRawDimensions(const f32v2& rawDimensions, DimensionType& dimensionType) { m_rawDimensions.first = rawDimensions; m_rawDimensions.second = dimensionType; updatePosition(); }
+            virtual void setMaxSize(const std::pair<f32v2, DimensionType>& maxSize) { m_maxSize = maxSize; updatePosition(); }
+            virtual void setMaxSize(const f32v2& maxSize, DimensionType& dimensionType) { m_maxSize.first = maxSize; m_maxSize.second = dimensionType; updatePosition(); }
+            virtual void setMaxWidth(f32 maxWidth) { m_maxSize.first.x = maxWidth; updatePosition(); }
+            virtual void setMaxHeight(f32 maxHeight) { m_maxSize.first.y = maxHeight; updatePosition(); }
+            virtual void setMaxSize(const std::pair<f32v2, DimensionType>& minSize) { m_minSize = minSize; updatePosition(); }
+            virtual void setMinSize(const f32v2& minSize) { m_minSize.first = minSize; updatePosition(); }
+            virtual void setMinWidth(f32 minWidth) { m_minSize.first.x = minWidth; updatePosition(); }
+            virtual void setMinHeight(f32 minHeight) { m_minSize.first.y = minHeight; updatePosition(); }
             virtual void setWidgetAlign(WidgetAlign align) { m_align = align; updatePosition(); }
             
         protected:
@@ -137,14 +163,15 @@ namespace vorb {
             /************************************************************************/
             WidgetAlign m_align = WidgetAlign::TOP_LEFT;
             AnchorStyle m_anchor; ///< The anchor data.
-            DockStyle m_dock = DockStyle::NONE; ///< The dock type.
             const vorb::graphics::SpriteFont* m_font = nullptr; ///< Font for rendering.
             UIRenderer* m_renderer = nullptr;
             IWidgetContainer* m_parent = nullptr; ///< Parent container
-            f32v2 m_minSize = f32v2(0.0f);
-            f32v2 m_maxSize = f32v2(FLT_MAX);
-            f32v2 m_positionPercentage = f32v2(-1.0f); ///< Position as percentage of parent dims
-            f32v2 m_dimensionsPercentage = f32v2(-1.0f); ///< Dims as percentage of parent dims
+            PositionType m_positionType = PositionType::STATIC;
+            std::pair<f32v2, DimensionType> m_minSize = std::pair<f32v2, DimensionType>(f32v2(0.0f), DimensionType::PIXEL);
+            std::pair<f32v2, DimensionType> m_maxSize = std::pair<f32v2, DimensionType>(f32v2(FLT_MAX), DimensionType::PIXEL);
+            // Is there a better way of doing this? E.g. moving position and dimensions out of IWidgetContainer?
+            std::pair<f32v2, DimensionType> m_rawPosition = std::pair<f32v2, DimensionType>(f32v2(0.0f), DimensionType::PIXEL);
+            std::pair<f32v2, DimensionType> m_rawDimensions = std::pair<f32v2, DimensionType>(f32v2(0.0f), DimensionType::PIXEL);
             volatile bool m_needsDrawableReload = false;
         };
     }

c45cff2f2aaeb08f1d7432ae72de917ce9f95a43
czaloj <czaloj@gmail.com>
2017-07-27 22:15:08 -0700

    Fix error from warning
    Compiler complained about loss of precision

--- src/ui/GameWindow.cpp
+++ src/ui/GameWindow.cpp
@@ -357,7 +357,7 @@ void vui::GameWindow::setScreenSize(i32 w, i32 h, bool overrideCheck /*= false*/
         m_displayMode.screenHeight = h;
 #if defined(VORB_IMPL_UI_SDL)
         SDL_SetWindowSize(VUI_WINDOW_HANDLE(m_window), m_displayMode.screenWidth, m_displayMode.screenHeight);
-        InputDispatcher::window.onResize({ w, h }); // TODO(Ben): This feels so dirty, but is necessary for LUA UI
+        InputDispatcher::window.onResize({ (ui32)w, (ui32)h }); // TODO(Ben): This feels so dirty, but is necessary for LUA UI
 #elif defined(VORB_IMPL_UI_GLFW)
         glfwSetWindowSize(VUI_WINDOW_HANDLE(m_window), m_displayMode.screenWidth, m_displayMode.screenHeight);
 #elif defined(VORB_IMPL_UI_SFML)

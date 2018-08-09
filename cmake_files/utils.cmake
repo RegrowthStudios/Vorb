6a0bc35d7add59ba897c9328e439aaa977108a1b
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-08-09 13:20:33 +0100

    Target C++14 standard as we are for SoA itself.

--- cmake_files/utils.cmake
+++ cmake_files/utils.cmake
@@ -39,8 +39,8 @@ macro(initialize_for_platform)
           # Other flags
           ${warnings}
       )
-      set(CMAKE_CXX_STANDARD 11)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+      set(CMAKE_CXX_STANDARD 14)
+      #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 
   elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
       set(warnings "/W4 /WX /EHsc")

3b68ffeae69b7341e93a4771e07e19e0725a0858
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-08-11 03:10:32 +0100

    Only 'remove' the class-memaccess warning from GCC as clang doesn't have that warning.

--- cmake_files/utils.cmake
+++ cmake_files/utils.cmake
@@ -28,7 +28,12 @@ macro(initialize_for_platform)
     endif()
   if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR
       "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-      set(warnings "-Wall -Wextra -Wno-class-memaccess -Wno-reorder -Wno-unknown-pragmas -Wno-attributes")
+      set(warnings "-Wall -Wextra -Wno-reorder -Wno-unknown-pragmas -Wno-attributes")
+      if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+        # This warning only exists for GCC.
+        set(warnings "${warnings} -Wno-class-memaccess")
+      endif()
+      
       ADD_DEFINITIONS(
 #          -std=c++11
 #          -stdlib=libc++

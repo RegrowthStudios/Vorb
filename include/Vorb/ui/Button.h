1a8f91a09d4566d6f8f1e6bdc6719a2dee24da24
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-09-17 20:57:33 +0100

    Complete button class.

--- include/Vorb/ui/Button.h
+++ include/Vorb/ui/Button.h
@@ -37,21 +37,38 @@ namespace vorb {
         public:
             /*! @brief Default constructor. */
             Button();
-            // /*! @brief Constructor that sets name, position, and dimensions.
-            // *
-            // * @param name: Name of the control.
-            // * @param destRect: Rectangle defining the position and dimensions as the tuple <x,y,w,h>.
-            // */
-            // Button(const nString& name, const f32v4& destRect = f32v4(0));
-            // /*! @brief Constructor that sets parent control, name, position, and dimensions.
-            // *
-            // * The control will be made a child of parent.
-            // *
-            // * @param parent: Parent control object.
-            // * @param name: Name of the control.
-            // * @param destRect: Rectangle defining the position and dimensions as the tuple <x,y,w,h>.
-            // */
-            // Button(IWidget* parent, const nString& name, const f32v4& destRect = f32v4(0));
+            /*! @brief Constructor that sets name, position, and dimensions.
+            *
+            * @param name: Name of the control.
+            * @param dimensions: Rectangle defining the position and dimensions as the tuple <x,y,w,h>.
+            */
+            Button(const nString& name, const f32v4& dimensions = f32v4(0.0f));
+            /*! \brief Constructor that sets name, position, and dimensions.
+             *
+             * \param name: Name of the control.
+             * \param position: The position of the widget.
+             * \param size: The size of the widget.
+             */
+            Button(const nString& name, const Length2& position, const Length2& size);
+            /*! @brief Constructor that sets parent control, name, position, and dimensions.
+            *
+            * The control will be made a child of parent.
+            *
+            * @param parent: Parent control object.
+            * @param name: Name of the control.
+            * @param dimensions: Rectangle defining the position and dimensions as the tuple <x,y,w,h>.
+            */
+            Button(IWidget* parent, const nString& name, const f32v4& dimensions = f32v4(0.0f));
+            /*! \brief Constructor that sets parent control, name, position, and dimensions.
+             *
+             * The widget will be made a child of parent.
+             *
+             * \param parent: Parent container object.
+             * \param name: Name of the control.
+             * \param position: The position of the widget.
+             * \param size: The size of the widget.
+             */
+            Button(IWidget* parent, const nString& name, const Length2& position, const Length2& size);
             /*! @brief Default destructor. */
             virtual ~Button(); // TODO(Ben): Maybe make abstract?
 
@@ -59,34 +76,24 @@ namespace vorb {
             */
             virtual void addDrawables() override;
 
-            /*! @brief Updates the position relative to parent */
-            // virtual void updatePosition() override;
-
             /************************************************************************/
             /* Getters                                                              */
             /************************************************************************/
-            virtual const VGTexture& getTexture() const { return m_drawableRect.getTexture(); }
-            virtual const vorb::graphics::SpriteFont* getFont() const override { return m_drawableText.getFont(); }
-            virtual const color4& getBackColor() const { return m_backColor1; }
-            virtual const color4& getBackHoverColor() const { return m_backHoverColor1; }
-            virtual const color4& getTextColor() const { return m_textColor; }
-            virtual const color4& getTextHoverColor() const { return m_textHoverColor; }
-            virtual const nString& getText() const { return m_drawableText.getText(); }
-            virtual const vg::TextAlign& getTextAlign() const { return m_drawableText.getTextAlign(); }
-            virtual const f32v2& getTextScale() const { return m_drawableText.getTextScale(); }
+            virtual                  const VGTexture& getTexture()        const { return m_drawableRect.getTexture();   }
+            virtual const vorb::graphics::SpriteFont* getFont()           const { return m_drawableText.getFont();      }
+            virtual                     const color4& getBackColor()      const { return m_backColor1;                  }
+            virtual                     const color4& getBackHoverColor() const { return m_backHoverColor1;             }
+            virtual                     const color4& getTextColor()      const { return m_textColor;                   }
+            virtual                     const color4& getTextHoverColor() const { return m_textHoverColor;              }
+            virtual                    const nString& getText()           const { return m_drawableText.getText();      }
+            virtual              const vg::TextAlign& getTextAlign()      const { return m_drawableText.getTextAlign(); }
+            virtual                      const f32v2& getTextScale()      const { return m_drawableText.getTextScale(); }
 
             /************************************************************************/
             /* Setters                                                              */
             /************************************************************************/
-            // virtual void setDestRect(const f32v4& destRect) override;
-            // virtual void setDimensions(const f32v2& dimensions) override;
-            // virtual void setFont(const vorb::graphics::SpriteFont* font) override;
-            // virtual void setHeight(f32 height) override;
-            // virtual void setPosition(const f32v2& position) override;
-            // virtual void setTexture(VGTexture texture);
-            // virtual void setWidth(f32 width) override;
-            // virtual void setX(f32 x) override;
-            // virtual void setY(f32 y) override;
+            virtual void setFont(const vorb::graphics::SpriteFont* font);
+            virtual void setTexture(VGTexture texture);
             virtual void setBackColor(const color4& color);
             virtual void setBackColorGrad(const color4& color1, const color4& color2, vg::GradientType grad);
             virtual void setBackHoverColor(const color4& color);
@@ -97,23 +104,29 @@ namespace vorb {
             virtual void setTextAlign(vg::TextAlign textAlign);
             virtual void setTextScale(const f32v2& textScale);
 
-        protected:            
+        protected:
+            virtual void calculateDrawables() override;
+
+            /*! Updates the colours set for the drawables. */
             virtual void updateColor();
+            /*! Updates the text position in the drawable. */
             virtual void updateTextPosition();
+
             virtual void refreshDrawables();
 
             virtual void onMouseMove(Sender s, const MouseMotionEvent& e) override;
             virtual void onMouseFocusLost(Sender s, const MouseEvent& e) override;
+
             /************************************************************************/
             /* Members                                                              */
             /************************************************************************/
-            DrawableRect m_drawableRect, m_drawnRect;
-            DrawableText m_drawableText, m_drawnText;
-            vg::GradientType gradBack = vg::GradientType::NONE, gradHover = vg::GradientType::NONE;
-            color4 m_backColor1 = color::LightGray, m_backColor2 = color::LightGray;
-            color4 m_backHoverColor1 = color::AliceBlue, m_backHoverColor2 = color::AliceBlue;
-            color4 m_textColor = color::Black, m_textHoverColor = color::Black;
-            const vg::SpriteFont* m_defaultFont = nullptr;
+            DrawableRect          m_drawableRect,    m_drawnRect;
+            DrawableText          m_drawableText,    m_drawnText;
+            vg::GradientType      m_gradBack,        m_gradHover;
+            color4                m_backColor1,      m_backColor2;
+            color4                m_backHoverColor1, m_backHoverColor2;
+            color4                m_textColor,       m_textHoverColor;
+            const vg::SpriteFont* m_defaultFont;
         };
     }
 }
--- src/ui/Button.cpp
+++ src/ui/Button.cpp
@@ -4,22 +4,45 @@
 #include "Vorb/ui/UIRenderer.h"
 #include "Vorb/ui/Viewport.h"
 
-vui::Button::Button() : Widget() {
-    updateColor();
+vui::Button::Button() : Button("") {
+    // Empty
 }
 
-// vui::Button::Button(const nString& name, VORB_UNUSED const f32v4& destRect /*= f32v4(0)*/) : Button() {
-//     m_name = name;
-//     // setDestRect(destRect);
-//     m_drawableRect.setPosition(getPosition());
-//     m_drawableRect.setDimensions(getSize());
-//     updateTextPosition();
-// }
+vui::Button::Button(const nString& name, const f32v4& dimensions /*= f32v4(0.0f)*/) : Widget(name, dimensions) {
+    m_gradBack        = vg::GradientType::NONE;
+    m_gradHover       = vg::GradientType::NONE;
+    m_backColor1      = color::LightGray;
+    m_backColor2      = color::LightGray;
+    m_backHoverColor1 = color::AliceBlue;
+    m_backHoverColor2 = color::AliceBlue;
+    m_textColor       = color::Black;
+    m_textHoverColor  = color::Black;
+    m_defaultFont     = nullptr;
 
-// vui::Button::Button(IWidget* parent, const nString& name, const f32v4& destRect /*= f32v4(0)*/) : Button(name, destRect) {
-//     parent->addWidget(this);
-//     m_parent = parent;
-// }
+    m_flags.needsDrawableRecalculation = true;
+}
+
+vui::Button::Button(const nString& name, const Length2& position, const Length2& size) : Widget(name, position, size) {
+    m_gradBack        = vg::GradientType::NONE;
+    m_gradHover       = vg::GradientType::NONE;
+    m_backColor1      = color::LightGray;
+    m_backColor2      = color::LightGray;
+    m_backHoverColor1 = color::AliceBlue;
+    m_backHoverColor2 = color::AliceBlue;
+    m_textColor       = color::Black;
+    m_textHoverColor  = color::Black;
+    m_defaultFont     = nullptr;
+
+    m_flags.needsDrawableRecalculation = true;
+}
+
+vui::Button::Button(IWidget* parent, const nString& name, const f32v4& dimensions /*= f32v4(0.0f)*/) : Button(name, dimensions) {
+    parent->addWidget(this);
+}
+
+vui::Button::Button(IWidget* parent, const nString& name, const Length2& position, const Length2& size) : Button(name, position, size) {
+    parent->addWidget(this);
+}
 
 vui::Button::~Button() {
     // Empty
@@ -45,137 +68,108 @@ void vui::Button::addDrawables() {
                   makeDelegate(*this, &Button::refreshDrawables));  
 }
 
-// void vui::Button::updatePosition() {
-//     Widget::updatePosition();
-//     m_drawableRect.setPosition(getPosition());
-//     m_drawableRect.setDimensions(getSize());
-//     m_drawableRect.setClipRect(m_clipRect);
-//     updateTextPosition();
-// }
+void vui::Button::setFont(const vorb::graphics::SpriteFont* font) {
+    m_drawableText.setFont(font);
+    
+    m_flags.needsDrawableRefresh = true;
+}
 
-// void vui::Button::setDestRect(const f32v4& destRect) {
-//     vui::Widget::setDestRect(destRect);
-//     m_drawableRect.setPosition(getPosition());
-//     m_drawableRect.setDimensions(getSize());
-//     refreshDrawables();
-// }
-
-// void vui::Button::setDimensions(const f32v2& dimensions) {
-//     Widget::setDimensions(dimensions);
-//     m_drawableRect.setDimensions(dimensions);
-//     updatePosition();
-// }
-
-// void vui::Button::setFont(const vorb::graphics::SpriteFont* font) {
-//     m_font = font;
-//     updatePosition();
-// }
-
-// void vui::Button::setHeight(f32 height) {
-//     Widget::setHeight(height);
-//     m_drawableRect.setHeight(height);
-//     updatePosition();
-// }
-
-// void vui::Button::setPosition(const f32v2& position) {
-//     Widget::setPosition(position);
-//     m_drawableRect.setPosition(m_position);
-//     updatePosition();
-// }
-
-// void vui::Button::setTexture(VGTexture texture) {
-//     m_drawableRect.setTexture(texture);
-//     refreshDrawables();
-// }
-
-// void vui::Button::setWidth(f32 width) {
-//     Widget::setWidth(width);
-//     m_drawableRect.setWidth(width);
-//     updatePosition();
-// }
-
-// void vui::Button::setX(f32 x) {
-//     Widget::setX(x);
-//     m_drawableRect.setX(m_position.x);
-//     updatePosition();
-// }
-
-// void vui::Button::setY(f32 y) {
-//     Widget::setY(y);
-//     m_drawableRect.setX(m_position.y);
-//     updatePosition();
-// }
+void vui::Button::setTexture(VGTexture texture) {
+    m_drawableRect.setTexture(texture);
+    
+    m_flags.needsDrawableRefresh = true;
+}
 
 void vui::Button::setBackColor(const color4& color) {
     m_backColor1 = m_backColor2 = color;
-    updateColor();
+    
+    m_flags.needsDrawableRecalculation = true;
 }
 
 void vui::Button::setBackColorGrad(const color4& color1, const color4& color2, vg::GradientType grad) {
     m_backColor1 = color1;
     m_backColor2 = color2;
-    gradBack = grad;
-    updateColor();
+    m_gradBack = grad;
+    
+    m_flags.needsDrawableRecalculation = true;
 }
 
 void vui::Button::setBackHoverColor(const color4& color) {
     m_backHoverColor1 = m_backHoverColor2 = color;
-    updateColor();
+    
+    m_flags.needsDrawableRecalculation = true;
 }
 
 void vui::Button::setBackHoverColorGrad(const color4& color1, const color4& color2, vg::GradientType grad) {
     m_backHoverColor1 = color1;
     m_backHoverColor2 = color2;
-    gradHover = grad;
-    updateColor();
+    m_gradHover = grad;
+    
+    m_flags.needsDrawableRecalculation = true;
 }
 
 void vui::Button::setText(const nString& text) {
     m_drawableText.setText(text);
-    updateTextPosition();
+    
+    m_flags.needsDrawableRefresh = true;
 }
 
 void vui::Button::setTextColor(const color4& color) {
     m_textColor = color;
-    updateColor();
+    
+    m_flags.needsDrawableRecalculation = true;
 }
 
 void vui::Button::setTextHoverColor(const color4& color) {
     m_textHoverColor = color;
-    updateColor();
+    
+    m_flags.needsDrawableRecalculation = true;
 }
 
 void vui::Button::setTextAlign(vg::TextAlign textAlign) {
     m_drawableText.setTextAlign(textAlign);
-    updateTextPosition();
+    
+    m_flags.needsDrawableRecalculation = true;
 }
 
 void vui::Button::setTextScale(const f32v2& textScale) {
     m_drawableText.setTextScale(textScale);
+    
+    m_flags.needsDrawableRefresh = true;
+}
+
+void vui::Button::calculateDrawables() {
+    m_drawableRect.setPosition(m_position);
+    m_drawableRect.setSize(m_size);
+    m_drawableRect.setClipRect(m_clipRect);
+    m_drawableText.setClipRect(m_clipRect);
+
+    updateColor();
+
     updateTextPosition();
+
+    m_flags.needsDrawableRefresh = true;
 }
 
 void vui::Button::updateColor() {
     if (m_flags.isMouseIn) {
         m_drawableRect.setColor1(m_backHoverColor1);
         m_drawableRect.setColor2(m_backHoverColor2);
-        m_drawableRect.setGradientType(gradBack);
+        m_drawableRect.setGradientType(m_gradBack);
         m_drawableText.setColor(m_textHoverColor);
     } else {
         m_drawableRect.setColor1(m_backColor1);
         m_drawableRect.setColor2(m_backColor2);
-        m_drawableRect.setGradientType(gradHover);
+        m_drawableRect.setGradientType(m_gradHover);
         m_drawableText.setColor(m_textColor);
     }
-    refreshDrawables();
 }
 
 void vui::Button::updateTextPosition() {
     const f32v2& dims = getSize();
-    const f32v2& pos = getPosition();
-    const vg::TextAlign& textAlign = getTextAlign();
+    const f32v2& pos  = getPosition();
 
-    m_drawableText.setClipRect(m_clipRect);
+    const vg::TextAlign& textAlign = getTextAlign();
 
     // TODO(Ben): Padding
     switch (textAlign) {
@@ -207,15 +201,15 @@ void vui::Button::updateTextPosition() {
             m_drawableText.setPosition(pos + dims / 2.0f);
             break;
     }
-    refreshDrawables();
 }
 
 void vui::Button::refreshDrawables() {
-
-    // Use renderer default font if we don't have a font
+    // Use renderer default font if we don't have a font.
     if (!m_drawableText.getFont()) {
         m_drawableText.setFont(m_defaultFont);
+
         m_drawnText = m_drawableText;
+
         m_drawableText.setFont(nullptr);
     } else {
         m_drawnText = m_drawableText;
@@ -225,7 +219,7 @@ void vui::Button::refreshDrawables() {
     m_drawnRect = m_drawableRect;
 }
 
-void vui::Button::onMouseMove(Sender s VORB_UNUSED, const MouseMotionEvent& e) {
+void vui::Button::onMouseMove(Sender s VORB_MAYBE_UNUSED, const MouseMotionEvent& e) {
     if (!m_flags.isEnabled) return;
     if (isInBounds((f32)e.x, (f32)e.y)) {
         if (!m_flags.isMouseIn) {
@@ -243,7 +237,7 @@ void vui::Button::onMouseMove(Sender s VORB_UNUSED, const MouseMotionEvent& e) {
     }
 }
 
-void vui::Button::onMouseFocusLost(Sender s VORB_UNUSED, const MouseEvent& e) {
+void vui::Button::onMouseFocusLost(Sender s VORB_MAYBE_UNUSED, const MouseEvent& e) {
     if (!m_flags.isEnabled) return;
     if (m_flags.isMouseIn) {
         m_flags.isMouseIn = false;

88fb397365f697a4e54799a00c2e6f6edbccaae5
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-11-02 21:42:26 +0000

    Add TODO.

--- src/ui/IWidget.cpp
+++ src/ui/IWidget.cpp
@@ -109,6 +109,7 @@ void vui::IWidget::disable() {
 }
 
 bool vui::IWidget::addWidget(IWidget* child) {
+    // TODO(Matthew): Should probably return false here instead.
     if (child->m_parent) {
         child->m_parent->removeWidget(child);
     }

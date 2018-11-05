4f552d2840ed30e460c6faf777a572d50504049f
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-11-05 16:22:51 +0000

    Update panel child offset on dimension update.

--- src/ui/Panel.cpp
+++ src/ui/Panel.cpp
@@ -106,6 +106,32 @@ void vui::Panel::setAutoScroll(bool autoScroll) {
 void vui::Panel::updateDimensions(f32 dt) {
     Widget::updateDimensions(dt);
 
+    if (m_autoScroll) {
+        if (m_sliders.horizontal.isEnabled()) {
+            // r == 0: offset_l = +(m_position.x - m_minX)
+            // r == 1: offset_r = -(m_maxX - m_position.x - m_size.x)
+            // We want a formula that combines these offsets:
+            //    offset = offset_l + r(offset_r - offset_l)
+
+            f32 r = m_sliders.horizontal.getValueScaled();
+
+            f32 offset = m_position.x - m_minX + r * ( m_minX - m_maxX + m_size.x );
+            setChildOffsetX(offset);
+        }
+
+        if (m_sliders.vertical.isEnabled()) {
+            // r == 0: offset_l = +(m_position.y - m_minY)
+            // r == 1: offset_r = -(m_maxY - m_position.y - m_size.y)
+            // We want a formula that combines these offsets:
+            //    offset = offset_l + r(offset_r - offset_l)
+
+            f32 r = m_sliders.vertical.getValueScaled();
+
+            f32 offset = m_position.y - m_minY + r * ( m_minY - m_maxY + m_size.y );
+            setChildOffsetY(offset);
+        }
+    }
+
     // updateSliders();
 }
 

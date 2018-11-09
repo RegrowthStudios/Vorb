e041f669b9ce09df7227a945f3600494155c4857
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-11-09 18:29:20 +0000

    Use CRTP to move a lot of general functions up to interface.

--- include/Vorb/script/IEnvironment.hpp
+++ include/Vorb/script/IEnvironment.hpp
@@ -36,11 +36,13 @@
 
 namespace vorb {
     namespace script {
-        using GenericScriptFunction = void*;
+        using GenericScriptFunction = void(*)();
+        using GenericCFunction      = void(*)();
 
+        template <typename DerivedEnvironment>
         class IEnvironment {
             using CFunctionList = std::vector<std::unique_ptr<DelegateBase>>;
-            using EventAdder    = void(*)(AutoDelegatePool&, GenericScriptFunction, EventBase*);
+            using EventAdder    = void(DerivedEnvironment::*)(GenericScriptFunction, EventBase*);
             struct EventData {
                 EventBase* event;
                 EventAdder adder;
@@ -52,26 +54,109 @@ namespace vorb {
              *
              * \param filepath The filepath from which to load the script.
              */
-            virtual bool load(const vio::Path& filepath) = 0;
+            bool load(const vio::Path& filepath) {
+                static_cast<DerivedEnvironment*>(this)->load(filepath);
+            }
 
             /*!
              * \brief Register an event with the script environment.
              * 
-             * The notion behind this is that we are able to add the subscriber 
+             * The notion behind this is that we are able to add the subscriber
              * to the appropriate event using a generic function pointer. This
              * way we need not use any templating here.
              *
              * \param name The name of the event for use by lua scripts when subscribing.
-             * \param eventData The event to be registered with a pointer to the 
+             * \param eventData The event to be registered with a pointer to the
              *                  function which adds subscribers to the event.
              */
             bool registerEvent(const nString& name, EventData eventData) {
                 return m_events.insert({ name, eventData }).second;
             }
+            /*!
+             * \brief Register an event with the script environment.
+             *
+             * The notion behind this is that we are able to add the subscriber
+             * to the appropriate event using a generic function pointer. This
+             * way we need not use any templating here.
+             *
+             * \param name The name of the event for use by lua scripts when subscribing.
+             * \param eventData The event to be registered with a pointer to the
+             *                  function which adds subscribers to the event.
+             */
+            template <typename ...Parameters>
+            bool registerEvent(const nString& name, Event<Parameters...>* event) {
+                return m_events.insert({ name, { static_cast<EventBase*>(event), &DerivedEnvironment::addScriptFunctionToEvent<Parameters...> } }).second;
+            }
+
+            /*!
+             * \brief Adds the provided LFunction to the provided Event.
+             *
+             * \tparam Parameters The list of parameter types provided by the Event on trigger.
+             *
+             * \param scriptFunction The Lua function to register to the event.
+             * \param eventBase The event to subscribe the Lua function to.
+             */
+            template <typename ...Parameters>
+            void addScriptFunctionToEvent(GenericScriptFunction scriptFunction, EventBase* eventBase) {
+                static_cast<DerivedEnvironment*>(this)->addScriptFunctionToEvent(scriptFunction, eventBase);
+            }
+
+            /*!
+             * \brief Add a C++ delegate as a hook with name "name" for Lua scripts to call.
+             *
+             * Note that this instance is here to act as a fake virtualisation of member template.
+             *
+             * \param name The name of the delegate to add.
+             * \param function The delegate to add.
+             */
+            template<typename ReturnType, typename ...Parameters, typename DelegateType = Delegate<ReturnType, Parameters...>>
+            void addCDelegate(const nString& name, DelegateType&& delegate) {
+                static_cast<DerivedEnvironment*>(this)->addCDelegate(name, std::move(delegate));
+            }
+
+            /*!
+             * \brief Adds the given value to the top of the lua stack.
+             *
+             * Note that this instance is here to act as a fake virtualisation of member template.
+             *
+             * \param name The key to give the value to be added to the Lua stack.
+             * \param value The value to add to the Lua stack.
+             */
+            template<typename Type>
+            bool addValue(const nString& name, Type value) {
+                return static_cast<DerivedEnvironment*>(this)->addValue(name, std::move(value));
+            }
+
+            /*!
+             * \brief Set the max length each script may be.
+             *
+             * \param length The max length.
+             */
+            void setMaxScriptLength(i32 length) { m_maxScriptLength = length; }
+
+            /*!
+             * \brief Get the max length each script may be.
+             *
+             * \return The max length.
+             */
+            i32 getMaxScriptLength() { return m_maxScriptLength; }
+
+            /*!
+             * \brief Sets a series of embedded lua tables to the top of the lua stack.
+             *
+             * Note that this instance is here to act as a fake virtualisation of member template.
+             *
+             * \param namespaces The namespaces to set.
+             */
+            template<typename ...Namespaces>
+            void setNamespaces(Namespaces... namespaces) {
+                static_cast<DerivedEnvironment*>(this)->setNamespaces(std::forward<Namespaces>(namespaces)...);
+            }
         protected:
-            CFunctionList    m_cFunctions;   ///< List of C++ functions registered with the script environment.
-            EventList        m_events;       ///< List of events registered with the script environment.
-            AutoDelegatePool m_listenerPool; ///< Manages Lua functions subscribed to events.
+            i32              m_maxScriptLength; ///< The maximum length a single script may be.
+            CFunctionList    m_cFunctions;      ///< List of C++ functions registered with the script environment.
+            EventList        m_events;          ///< List of events registered with the script environment.
+            AutoDelegatePool m_listenerPool;    ///< Manages Lua functions subscribed to events.
         };
     }
 }

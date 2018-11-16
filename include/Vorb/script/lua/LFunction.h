d4e1c9a408daaba6119e028e80d78d7f6af4b948
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-11-16 17:16:43 +0000

    Implement single return type LFunctions.

--- include/Vorb/script/lua/LFunction.h
+++ include/Vorb/script/lua/LFunction.h
@@ -30,6 +30,10 @@ namespace vorb {
         namespace lua {
             using Index = i32;
 
+            // Forward declaration of SRLFunction.
+            template<typename ReturnType>
+            class SRLFunction;
+
             // Forward declaration of RLFunction.
             template<typename ...ReturnTypes>
             class RLFunction;
@@ -41,6 +45,8 @@ namespace vorb {
              * and return values.
              */
             class LFunction {
+                template<typename ReturnType>
+                friend class SRLFunction;
                 template<typename ...ReturnTypes>
                 friend class RLFunction;
             public:
@@ -56,6 +62,14 @@ namespace vorb {
                  */
                 LFunction(Handle state, const nString& name, Index index) :
                     m_state(state), m_name(name), m_index(index)
+                { /* EMPTY */ }
+                /*!
+                 * \brief Copy Constructor
+                 *
+                 * \param lFunction The RLFunction instance to copy.
+                 */
+                LFunction(const LFunction& lFunction) :
+                    m_state(lFunction.m_state), m_name(lFunction.m_name), m_index(lFunction.m_index)
                 { /* EMPTY */ }
                 LFunction(LFunction&& lFunction) :
                     m_state(lFunction.m_state), m_name(lFunction.m_name), m_index(lFunction.m_index)
@@ -101,6 +115,16 @@ namespace vorb {
                 bool operator==(const LFunction& rhs) const;
 
                 /*!
+                 * \brief Creates an SRLFunction wrapper, which expects the wrapped lua function to return the type of ReturnType.
+                 *
+                 * \tparam ReturnTypes The list of types of the expected values to be returned from the Lua function.
+                 *
+                 * \return Instance of the RLFunction type, ready for calling.
+                 */
+                template<typename ReturnType>
+                SRLFunction<ReturnType> withSingleReturn() const;
+
+                /*!
                  * \brief Creates an RLFunction wrapper, which expects the wrapped lua function to return the types of ReturnTypes.
                  *
                  * \tparam ReturnTypes The list of types of the expected values to be returned from the Lua function.
@@ -130,6 +154,61 @@ namespace vorb {
                 Index   m_index; ///< The index of the function in Lua registry.
             };
 
+            /*! \brief Variant of the wrapper that specifies a single expected return type. */
+            template<typename ReturnType>
+            class SRLFunction : public LFunction {
+            public:
+                SRLFunction() :
+                    LFunction()
+                { /* EMPTY */ }
+                /*!
+                 * \brief Constructor
+                 *
+                 * \param state The Lua stack.
+                 * \param name The name of the function.
+                 * \param index The index of the function in the Lua registry.
+                 */
+                SRLFunction(Handle state, const nString& name, Index index) :
+                    LFunction(state, name, index)
+                { /* EMPTY */ }
+                /*!
+                 * \brief Copy Constructor
+                 *
+                 * \param lFunction The RLFunction instance to copy.
+                 */
+                SRLFunction(const LFunction& lFunction) :
+                    LFunction(lFunction.m_state, lFunction.m_name, lFunction.m_index)
+                { /* EMPTY */ }
+                // We don't want to be unreferencing here as this class is simply a copy
+                // of an LFunction with a return type.
+                ~SRLFunction()
+                { /* EMPTY */ }
+
+
+                /*!
+                 * \brief Functions to call the wrapped lua function.
+                 *
+                 * \tparam Parameters The list of types of the parameters to be passed to the function.
+                 *
+                 * \param returnValues The tuple to place returned values into.
+                 * \param parameters The parameters passed to the function.
+                 */
+                template<typename ...Parameters>
+                ReturnType call(Parameters... parameters) const;
+                /*!
+                 * \brief Functions to call the wrapped lua function.
+                 *
+                 * operator() is an alias of call.
+                 *
+                 * \tparam Parameters The list of types of the parameters to be passed to the function.
+                 *
+                 * \param returnValues The tuple to place returned values into.
+                 * \param parameters The parameters passed to the function.
+                 */
+                template<typename ...Parameters>
+                ReturnType operator()(Parameters... parameters) const;
+            };
+
             /*! \brief Variant of the wrapper that specifies expected return types. */
             template<typename ...ReturnTypes>
             class RLFunction : public LFunction {
@@ -150,7 +229,7 @@ namespace vorb {
                 /*!
                  * \brief Copy Constructor
                  *
-                 * \param lFunction The LFunction instance to copy.
+                 * \param lFunction The RLFunction instance to copy.
                  */
                 RLFunction(const LFunction& lFunction) :
                     LFunction(lFunction.m_state, lFunction.m_name, lFunction.m_index)
@@ -250,9 +329,61 @@ inline void vscript::lua::LFunction::operator()(Parameters... parameters) const 
     call(std::forward<Parameters>(parameters)...);
 }
 
+template<typename ReturnType>
+inline vscript::lua::SRLFunction<ReturnType> vscript::lua::LFunction::withSingleReturn() const {
+    return SRLFunction<ReturnType>(*this);
+}
+
 template<typename ...ReturnTypes>
 inline vscript::lua::RLFunction<ReturnTypes...> vscript::lua::LFunction::withReturn() const {
     return RLFunction<ReturnTypes...>(*this);
+}
+
+template<typename ReturnType>
+template<typename ...Parameters>
+inline ReturnType vscript::lua::SRLFunction<ReturnType>::call(Parameters ...parameters) const {
+    // Put the script function table in the lua registry on to the top of the lua stack.
+    lua_getfield(m_state, LUA_REGISTRYINDEX, SCRIPT_FUNCTION_TABLE);
+    // Get a raw accessor to the wrapped script function.
+    lua_rawgeti(m_state, -1, m_index);
+
+    // Push the parameters of this call onto the stack.
+    pushParams(m_state, parameters...);
+
+    // Store the depth of the stack before calling the lua function.
+    i32 initialDepth = lua_gettop(m_state);
+
+    // Call the function with the provided parameters.
+    if (lua_pcall(m_state, sizeof...(Parameters), ValueMediator<ReturnType>::getValueCount(), 0) != 0) {
+        // Print any errors that arose.
+        dumpErrors();
+    }
+
+    ReturnType returnVal;
+    // Try to pop all of the expected return values off of the stack.
+    if (!ValueMediator<ReturnType>::tryPop(m_state, returnVal)) {
+        // TODO(Matthew): Unify error handling across the environment?
+        // Handle the failure.
+        //std::cout << "Uh oh, could not get the return values as expected." << std::endl;
+        printf("Uh oh, could not get the return values as expected.");
+
+        // Calculate the change in the stack depth.
+        i32 deltaDepth = lua_gettop(m_state) - initialDepth;
+        // Pop the remaining returned values off the stack.
+        lua_pop(m_state, deltaDepth);
+    }
+
+    // Pop the script function table off the stack.
+    lua_pop(m_state, 1);
+
+    // Return our result.
+    return returnVal;
+}
+
+template<typename ReturnType>
+template<typename ...Parameters>
+inline ReturnType vscript::lua::SRLFunction<ReturnType>::operator()(Parameters ...parameters) const {
+    return call(std::forward<Parameters>(parameters)...);
 }
 
 template<typename ...ReturnTypes>

245224aaf8d08618cc46860786280725032efcb0
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2019-06-29 02:11:45 +0100

    Utilise helper parsing functions.

--- src/ui/widgets/yaml/Label.cpp
+++ src/ui/widgets/yaml/Label.cpp
@@ -3,33 +3,39 @@
 
 #include "Vorb/graphics/TextureCache.h"
 #include "Vorb/ui/widgets/Label.h"
+#include "Vorb/ui/widgets/yaml/helper.hpp"
 #include "Vorb/ui/widgets/yaml/TextWidget.h"
 
 bool vui::parseLabelEntry(keg::ReadContext& context, vui::Label* label, const nString& name, keg::Node value, Delegate<vui::IWidget*, const nString&, keg::Node>* widgetParser, vg::TextureCache* textureCache) {
-    if (name == "color") {
-        
-    } else if (name == "hover_color") {
-        
-    } else if (name == "texture") {
-        if (keg::getType(value) != keg::NodeType::VALUE) return false;
+    if (name == "bg_color") {
+        color4 color;
+        if (!parseColor(value, color)) return false;
 
-        nString texturePath = value->data[0].as<nString>();
+        label->setLabelColor(color);
+    } else if (name == "bg_hover_color") {
+        color4 color;
+        if (!parseColor(value, color)) return false;
+
+        label->setLabelHoverColor(color);
+    } else if (name == "bg_texture") {
+        nString texturePath;
+        if (!parseString(value, texturePath)) return false;
 
         vg::Texture tex = textureCache->addTexture(texturePath);
 
         label->setLabelTexture(tex.id);
-    } else if (name == "hover_texture") {
-        if (keg::getType(value) != keg::NodeType::VALUE) return false;
-
-        nString texturePath = value->data[0].as<nString>();
+    } else if (name == "bg_hover_texture") {
+        nString texturePath;
+        if (!parseString(value, texturePath)) return false;
 
         vg::Texture tex = textureCache->addTexture(texturePath);
 
         label->setLabelHoverTexture(tex.id);
-    } else if (name == "text_color") {
-        
     } else if (name == "text_hover_color") {
+        color4 color;
+        if (!parseColor(value, color)) return false;
 
+        label->setTextHoverColor(color);
     }
 
     return vui::parseTextWidgetEntry(context, label, name, value, widgetParser);

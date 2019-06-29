c98b979c01f6acd153421b7a9629e4af59599f50
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2019-06-29 02:12:02 +0100

    Implement YAML loading for button widget.

---
+++ src/ui/widgets/yaml/Button.cpp
@@ -0,0 +1,56 @@
+#include "Vorb/stdafx.h"
+#include "Vorb/ui/widgets/yaml/Button.h"
+
+#include "Vorb/graphics/TextureCache.h"
+#include "Vorb/ui/widgets/Button.h"
+#include "Vorb/ui/widgets/yaml/helper.hpp"
+#include "Vorb/ui/widgets/yaml/TextWidget.h"
+
+bool vui::parseButtonEntry(keg::ReadContext& context, vui::Button* button, const nString& name, keg::Node value, Delegate<vui::IWidget*, const nString&, keg::Node>* widgetParser, vg::TextureCache* textureCache) {
+    if (name == "bg_color") {
+        color4 color;
+        if (!parseColor(value, color)) return false;
+
+        button->setBackColor(color);
+    } else if (name == "bg_hover_color") {
+        color4 color;
+        if (!parseColor(value, color)) return false;
+
+        button->setBackHoverColor(color);
+    } else if (name == "bg_gradient") {
+        color4           color1, color2;
+        vg::GradientType gradType;
+        if (!parseGradient(value, color1, color2, gradType)) return false;
+
+        button->setBackColorGrad(color1, color2, gradType);
+    } else if (name == "bg_hover_gradient") {
+        color4           color1, color2;
+        vg::GradientType gradType;
+        if (!parseGradient(value, color1, color2, gradType)) return false;
+
+        button->setBackHoverColorGrad(color1, color2, gradType);
+    } else if (name == "bg_texture") {
+        if (keg::getType(value) != keg::NodeType::VALUE) return false;
+
+        nString texturePath = value->data[0].as<nString>();
+
+        vg::Texture tex = textureCache->addTexture(texturePath);
+
+        button->setTexture(tex.id);
+    } else if (name == "bg_hover_texture") {
+        if (keg::getType(value) != keg::NodeType::VALUE) return false;
+
+        nString texturePath = value->data[0].as<nString>();
+
+        vg::Texture tex = textureCache->addTexture(texturePath);
+
+        button->setHoverTexture(tex.id);
+    } else if (name == "text_hover_color") {
+        color4 color;
+        if (!parseColor(value, color)) return false;
+
+        button->setTextHoverColor(color);
+    }
+
+    return vui::parseTextWidgetEntry(context, button, name, value, widgetParser);
+}

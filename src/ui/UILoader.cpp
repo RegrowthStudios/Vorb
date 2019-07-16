d648eedbb286d754b6c8bea51fd2d3faf1f0c911
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2019-07-16 16:57:40 +0100

    Make this work.

--- src/ui/UILoader.cpp
+++ src/ui/UILoader.cpp
@@ -6,16 +6,23 @@
 #include "Vorb/io/Keg.h"
 #include "Vorb/io/Path.h"
 #include "Vorb/io/YAMLImpl.h"
+#include "Vorb/ui/widgets/Button.h"
+#include "Vorb/ui/widgets/CheckBox.h"
+#include "Vorb/ui/widgets/ComboBox.h"
 #include "Vorb/ui/widgets/IWidget.h"
+#include "Vorb/ui/widgets/Label.h"
+#include "Vorb/ui/widgets/Panel.h"
+#include "Vorb/ui/widgets/Slider.h"
 #include "Vorb/ui/widgets/Widget.h"
+#include "Vorb/ui/widgets/WidgetList.h"
 #include "Vorb/ui/widgets/Viewport.h"
-#include "Vorb/ui/widgets/yaml/Widget.h"
-#include "Vorb/ui/widgets/yaml/Label.h"
+#include "Vorb/ui/widgets/yaml/Button.h"
 #include "Vorb/ui/widgets/yaml/CheckBox.h"
 #include "Vorb/ui/widgets/yaml/ComboBox.h"
 #include "Vorb/ui/widgets/yaml/Label.h"
 #include "Vorb/ui/widgets/yaml/Panel.h"
 #include "Vorb/ui/widgets/yaml/Slider.h"
+#include "Vorb/ui/widgets/yaml/Widget.h"
 #include "Vorb/ui/widgets/yaml/WidgetList.h"
 
 // TODO(Matthew): Could be a lot cleaner if done entirely through Keg, but as is Keg doesn't seem to support
@@ -24,19 +31,12 @@
 
 // TODO(Matthew): On failed parse, ensure clean up covers all thus-far created widgets!
 
-vui::IWidget* parseWidget(keg::ReadContext& context, const nString& type, keg::Node node, vui::UILoader::WidgetParser* customWidgetParser) {
+// TODO(Matthew): Can we put all the widgets in the same container to reduce fragmentation of their data?
+//                Would involve creating some kind of generic container that is element-wise sized on max-sized widget kind.
+
+vui::IWidget* parseWidget(keg::ReadContext& context, const nString& type, keg::Node node, vui::UILoader::WidgetParser* parser, vg::TextureCache* textureCache) {
     // Ensure node is a map.
     if (keg::getType(node) != keg::NodeType::MAP) return nullptr;
-
-    // Define a functor that provides both default parsing and, on failure of default parsing, pass off 
-    // to custom parser.
-    auto widgetParser = makeFunctor([&](const nString& type, keg::Node node) {
-        vui::IWidget* widget = parseWidget(context, type, node, customWidgetParser);
-
-        if (!widget && customWidgetParser) widget = customWidgetParser->invoke(type, node);
-
-        return widget;
-    });
 
     // For each entry in the map, create the appropriate widget and send it off for parsing to the correct parser.
     bool success = true;
@@ -45,37 +45,37 @@ vui::IWidget* parseWidget(keg::ReadContext& context, const nString& type, keg::N
     if (type == "button") {
         widget = new vui::Button();
         processEntry = makeFunctor([&](Sender, const nString& key, keg::Node value) {
-            success = success && vui::parseButtonEntry(context, static_cast<vui::Button*>(widget), key, value, widgetParser);
+            success = success && vui::parseButtonEntry(context, static_cast<vui::Button*>(widget), key, value, parser, textureCache);
         });
     } else if (type == "checkbox") {
         widget = new vui::CheckBox();
         processEntry = makeFunctor([&](Sender, const nString& key, keg::Node value) {
-            success = success && vui::parseCheckBoxEntry(context, static_cast<vui::CheckBox*>(widget), key, value, widgetParser);
+            success = success && vui::parseCheckBoxEntry(context, static_cast<vui::CheckBox*>(widget), key, value, parser, textureCache);
         });
     } else if (type == "combobox") {
         widget = new vui::ComboBox();
         processEntry = makeFunctor([&](Sender, const nString& key, keg::Node value) {
-            success = success && vui::parseComboBoxEntry(context, static_cast<vui::ComboBox*>(widget), key, value, widgetParser);
+            success = success && vui::parseComboBoxEntry(context, static_cast<vui::ComboBox*>(widget), key, value, parser, textureCache);
         });
     } else if (type == "label") {
         widget = new vui::Label();
         processEntry = makeFunctor([&](Sender, const nString& key, keg::Node value) {
-            success = success && vui::parseLabelEntry(context, static_cast<vui::Label*>(widget), key, value, widgetParser);
+            success = success && vui::parseLabelEntry(context, static_cast<vui::Label*>(widget), key, value, parser, textureCache);
         });
     } else if (type == "panel") {
         widget = new vui::Panel();
         processEntry = makeFunctor([&](Sender, const nString& key, keg::Node value) {
-            success = success && vui::parsePanelEntry(context, static_cast<vui::Panel*>(widget), key, value, widgetParser);
+            success = success && vui::parsePanelEntry(context, static_cast<vui::Panel*>(widget), key, value, parser, textureCache);
         });
     } else if (type == "slider") {
         widget = new vui::Slider();
         processEntry = makeFunctor([&](Sender, const nString& key, keg::Node value) {
-            success = success && vui::parseSliderEntry(context, static_cast<vui::Slider*>(widget), key, value, widgetParser);
+            success = success && vui::parseSliderEntry(context, static_cast<vui::Slider*>(widget), key, value, parser, textureCache);
         });
     } else if (type == "widget_list") {
         widget = new vui::WidgetList();
         processEntry = makeFunctor([&](Sender, const nString& key, keg::Node value) {
-            success = success && vui::parseWidgetListEntry(context, static_cast<vui::WidgetList*>(widget), key, value, widgetParser);
+            success = success && vui::parseWidgetListEntry(context, static_cast<vui::WidgetList*>(widget), key, value, parser, textureCache);
         });
     } else {
         return nullptr;
@@ -91,31 +91,23 @@ vui::IWidget* parseWidget(keg::ReadContext& context, const nString& type, keg::N
     }
 }
 
-bool parseViewport(keg::ReadContext& context, vui::Viewport* viewport, keg::Node node, vui::UILoader::WidgetParser* customWidgetParser) {
+bool parseViewport(keg::ReadContext& context, vui::Viewport* viewport, keg::Node node, vui::UILoader::WidgetParser* parser) {
     // Ensure node is a map.
     if (keg::getType(node) != keg::NodeType::MAP) return false;
-
-    // Define a functor that provides both default parsing and, on failure of default parsing, pass off 
-    // to custom parser.
-    auto widgetParser = makeFunctor([&](const nString& type, keg::Node node) {
-        vui::IWidget* widget = parseWidget(context, type, node, customWidgetParser);
-
-        if (!widget && customWidgetParser) widget = customWidgetParser->invoke(type, node);
-
-        return widget;
-    });
 
     // For each entry in the map, send it off for parsing.
     bool success = true;
     auto processViewportEntry = makeFunctor([&](Sender, const nString& key, keg::Node value) {
-        success = success && vui::parseWidgetEntry(context, viewport, key, value, &widgetParser);
+        success = success && vui::parseWidgetEntry(context, viewport, key, value, parser);
     });
     context.reader.forAllInMap(node, &processViewportEntry);
 
     return success;
 }
 
-bool vui::UILoader::load(const vio::IOManager& iom, const cString filepath, OUT Viewport& viewport, WidgetParser* customWidgetParser /*= nullptr*/) {
+
+
+bool vui::UILoader::loadFromYAML(const vio::IOManager& iom, const cString filepath, vg::TextureCache* textureCache, Viewport* viewport, WidgetParser* customWidgetParser /*= nullptr*/) {
     // Check if the filepath given can be resolved.
     vio::Path path;
     if (!iom.resolvePath(filepath, path)) return false;
@@ -130,15 +122,22 @@ bool vui::UILoader::load(const vio::IOManager& iom, const cString filepath, OUT 
     context.env = keg::getGlobalEnvironment();
     context.reader.init(data.c_str());
 
+    // TODO(Matthew): Do we want to preferentially pass off to custom parser?
+    // Define a functor that provides both default parsing and, on failure of default parsing, pass off 
+    // to custom parser.
+    auto widgetParser = makeFunctor([&](const nString& type, keg::Node node) {
+        vui::IWidget* widget = parseWidget(context, type, node, customWidgetParser, textureCache);
+
+        if (!widget && customWidgetParser) widget = customWidgetParser->invoke(type, node);
+
+        return widget;
+    });
+
     // Get first node entry and pass on for parsing as viewport.
     keg::Node node = context.reader.getFirst();
-    bool res = parseViewport(context, &viewport, node, customWidgetParser);
+    bool res = parseViewport(context, viewport, node, &widgetParser);
 
     // Dispose reader and return result.
     context.reader.dispose();
     return res;
-}
-
-bool vui::UILoader::save(const cString filepath, const Viewport& viewport) {
-
 }

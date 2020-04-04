f1530bf121016c6ade268bdcad7ace141bc56092
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2020-04-04 02:46:45 +0100

    Create first pass of load order manager.

---
+++ include/Vorb/mod/LoadOrder.h
@@ -0,0 +1,92 @@
+//
+// LoadOrder.h
+// Vorb Engine
+//
+// Created by Matthew Marshall on 3 April 2020
+// Copyright 2020 Regrowth Studios
+// MIT License
+//
+
+/*! \file LoadOrder.h
+ * \brief Handles load order of mods.
+ */
+
+#pragma once
+
+#ifndef Vorb_LoadOrderManager_h__
+//! @cond DOXY_SHOW_HEADER_GUARDS
+#define Vorb_LoadOrderManager_h__
+//! @endcond
+
+#ifndef VORB_USING_PCH
+#include "Vorb/types.h"
+#endif // !VORB_USING_PCH
+
+#include "Vorb/io/IOManager.h"
+#include "Vorb/io/Keg.h"
+#include "Vorb/mod/Mod.h"
+
+namespace vorb {
+    namespace mod {
+        struct LoadOrderProfile {
+            // TODO(Matthew): Fill this out with more data points as we go.
+            nString name; ///< Name of load order profile.
+            nString createdTimestamp; ///< Profile creation timestamp.
+            nString lastModifiedTimestamp; ///< Profile last modified timestamp.
+
+            Array<nString> mods; ///< List of mods by name in load order (first in
+                                 ///  array is therefore lowest priority: the later
+                                 ///  mods will overwrite the earlier ones).
+        };
+        using LoadOrderProfiles = Array<LoadOrderProfile>;
+        struct LoadOrders {
+            nString currentProfileName;
+            LoadOrderProfiles profiles;
+        };
+
+        const nString LOAD_ORDER_PROFILES_FILENAME = "load_order_profiles.yaml";
+
+        class LoadOrderManager {
+        public:
+            LoadOrderManager();
+
+            /*!
+             * \brief Initialises the load order manager with the IOManager
+             * needed to acquire load orders.
+             * 
+             * Note: the IOManager instance provided should already have its
+             * search directory set to the location that load order profiles
+             * may be located.
+             *
+             * \param ioManager: The IOManager with which to acquire load orders.
+             */
+            void init(CALLER_DELETE vio::IOManager* ioManager);
+            /*! \brief Disposes the load order manager.
+             */
+            void dispose();
+
+            const LoadOrderProfile& getLoadOrder(const nString& name) const;
+            const LoadOrderProfile& getCurrentLoadOrder() const;
+            const LoadOrderProfiles& getAllLoadOrders() const;
+        protected:
+            /*! \brief Acquires all load order profiles currently specified.
+             */
+            void acquireLoadOrders();
+
+            vio::IOManager* m_ioManager; ///< The IO manager with which load orders are acquired.
+
+            LoadOrders m_loadOrders; ///< The list of load order profiles.
+            /*!
+             * The currently active load order profile. If none is active,
+             * then this will be set to nullptr.
+             */
+            LoadOrderProfile* m_currentLoadOrder;
+        };
+    }
+}
+namespace vmod = vorb::mod;
+
+KEG_TYPE_DECL(LoadOrderProfile);
+KEG_TYPE_DECL(LoadOrders);
+
+#endif // !Vorb_LoadOrderManager_h__
---
+++ src/mod/LoadOrder.cpp
@@ -0,0 +1,68 @@
+#include "Vorb/stdafx.h"
+#include "Vorb/mod/LoadOrder.h"
+
+KEG_TYPE_DEF(LoadOrderProfile, vmod::LoadOrderProfile, kt) {
+    using namespace keg;
+    kt.addValue("name", Value::basic(offsetof(vmod::LoadOrderProfile, name), BasicType::STRING));
+    kt.addValue("createdTimestamp", Value::basic(offsetof(vmod::LoadOrderProfile, createdTimestamp), BasicType::STRING));
+    kt.addValue("lastModifiedTimestamp", Value::basic(offsetof(vmod::LoadOrderProfile, lastModifiedTimestamp), BasicType::STRING));
+}
+
+KEG_TYPE_DEF(LoadOrders, vmod::LoadOrders, kt) {
+    using namespace keg;
+    kt.addValue("currentProfileName", Value::basic(offsetof(vmod::LoadOrders, currentProfileName), BasicType::STRING));
+    kt.addValue("profiles", Value::array(offsetof(vmod::LoadOrders, profiles), Value::custom(0, "LoadOrderProfile")));
+}
+
+vmod::LoadOrderManager::LoadOrderManager() :
+    m_currentLoadOrder(nullptr) {
+        // Empty.
+}
+
+void vmod::LoadOrderManager::init(vio::IOManager* ioManager) {
+    m_ioManager = ioManager;
+    m_ioManager->setSearchOnly(true);
+
+    acquireLoadOrders();
+}
+
+void vmod::LoadOrderManager::dispose() {
+    m_ioManager = nullptr;
+    m_currentLoadOrder = nullptr;
+
+    m_loadOrders = {};
+}
+
+const vmod::LoadOrderProfile& vmod::LoadOrderManager::getLoadOrder(const nString& name) const {
+    for (auto& loadOrder: m_loadOrders.profiles) {
+        if (loadOrder.name == name) {
+            return loadOrder;
+        }
+    }
+}
+
+const vmod::LoadOrderProfile& vmod::LoadOrderManager::getCurrentLoadOrder() const {
+    return *m_currentLoadOrder;
+}
+
+const vmod::LoadOrderProfiles& vmod::LoadOrderManager::getAllLoadOrders() const {
+    return m_loadOrders.profiles;
+}
+
+void vmod::LoadOrderManager::acquireLoadOrders() {
+    // Get load order profiles from file.
+    cString profilesRaw = m_ioManager->readFileToString(LOAD_ORDER_PROFILES_FILENAME);
+    if (profilesRaw == nullptr) return;
+
+    // Attempt to parse load order profiles.
+    if (keg::parse(&m_loadOrders, profilesRaw, "LoadOrders") != keg::Error::NONE) {
+        printf("Could not parse load order profiles.");
+    }
+
+    // Create pointer to current load order.
+    for (auto& loadOrder: m_loadOrders.profiles) {
+        if (loadOrder.name == m_loadOrders.currentProfileName) {
+            m_currentLoadOrder = &loadOrder;
+        }
+    }
+}

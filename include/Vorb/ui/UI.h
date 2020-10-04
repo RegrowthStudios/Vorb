ceda9c0500ef9c15cc7bb4b54d5d809f31768d12
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2020-10-05 00:56:39 +0100

    Enable UIs to be build obtain view creation script envs from registry or context from builder.

--- include/Vorb/ui/UI.h
+++ include/Vorb/ui/UI.h
@@ -200,6 +200,78 @@ namespace vorb {
             : public UIBase {
         public:
             /*!
+             * \brief Initialises the UI with the necessary services to acquire files, textures, render fonts and othe spritesm etc.
+             *
+             * \param ownerScreen: The screen to which this UI is attached.
+             * \param window: The window in which the game is embedded.
+             * \param ioManager: The IO manager for acquiring files such as YAML files and scripts.
+             * \param textureCache: The cache used for obtaining textures.
+             * \param fontCache: The font used for rendering text in the UI.
+             * \param spriteBatch: The batcher used for rendering other sprites of the UI.
+             */
+            virtual void init(
+                       IGameScreen* ownerScreen,
+                  const GameWindow* window,
+                vio::IOManagerBase* ioManager,
+                  vg::TextureCache* textureCache,
+                     vg::FontCache* fontCache,
+                   vg::SpriteBatch* spriteBatch
+            ) override {
+                init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch, nullptr, "");
+            }
+            /*!
+             * \brief Initialises the UI with the necessary services to acquire files, textures, render fonts and othe spritesm etc.
+             *
+             * \param ownerScreen: The screen to which this UI is attached.
+             * \param window: The window in which the game is embedded.
+             * \param ioManager: The IO manager for acquiring files such as YAML files and scripts.
+             * \param textureCache: The cache used for obtaining textures.
+             * \param fontCache: The font used for rendering text in the UI.
+             * \param spriteBatch: The batcher used for rendering other sprites of the UI.
+             * \param scriptEnvBuilder: The callback to use to build the context of an isolated script environment.
+             */
+            void init(
+                                                         IGameScreen* ownerScreen,
+                                                    const GameWindow* window,
+                                                  vio::IOManagerBase* ioManager,
+                                                    vg::TextureCache* textureCache,
+                                                       vg::FontCache* fontCache,
+                                                     vg::SpriteBatch* spriteBatch,
+                vscript::ScriptEnvironmentBuilder<ScriptEnvironment>* scriptEnvBuilder
+            ) {
+                UIBase::init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch);
+
+                m_scriptEnvironmentBuilder = scriptEnvBuilder;
+            }
+            /*!
+             * \brief Initialises the UI with the necessary services to acquire files, textures, render fonts and othe spritesm etc.
+             *
+             * \param ownerScreen: The screen to which this UI is attached.
+             * \param window: The window in which the game is embedded.
+             * \param ioManager: The IO manager for acquiring files such as YAML files and scripts.
+             * \param textureCache: The cache used for obtaining textures.
+             * \param fontCache: The font used for rendering text in the UI.
+             * \param spriteBatch: The batcher used for rendering other sprites of the UI.
+             * \param scriptEnvRegistry: The registry from which to obtain script environments.
+             * \param scriptEnvGroup: The group within the registry from which to obtain script environments.
+             */
+            void init(
+                                                    IGameScreen* ownerScreen,
+                                               const GameWindow* window,
+                                             vio::IOManagerBase* ioManager,
+                                               vg::TextureCache* textureCache,
+                                                  vg::FontCache* fontCache,
+                                                vg::SpriteBatch* spriteBatch,
+                vscript::EnvironmentRegistry<ScriptEnvironment>* scriptEnvRegistry,
+                                                  const nString& scriptEnvGroup
+            ) {
+                UIBase::init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch);
+
+                m_scriptEnvironmentRegistry = scriptEnvRegistry;
+                m_scriptEnvGroup = scriptEnvGroup;
+            }
+
+            /*!
              * \brief Creates a new view in this UI from the named script.
              *
              * \param name The name of the new view.
@@ -211,11 +283,18 @@ namespace vorb {
             Viewport* makeViewFromScript(const nString& name, ZIndex zIndex, const nString& filepath) {
                 Viewport* view = makeView(name, zIndex);
 
-                ScriptEnvironment scriptEnv;
-                scriptEnv.init();
+                // Prepare script environment, using registry if provided, builder if that was provided.
+                ScriptEnvironment* scriptEnv = nullptr;
+                if (m_scriptEnvGroup != "") {
+                    scriptEnv = m_scriptEnvironmentRegistry->getScriptEnv(m_scriptEnvGroup);
+                } else {
+                    scriptEnv = new ScriptEnvironment();
+                    scriptEnv->init();
 
-                ViewScriptContext::injectInto(scriptEnv);
+                    if (m_scriptEnvironmentBuilder != nullptr) m_scriptEnvironmentBuilder->invoke(scriptEnv);
+                }
 
+                // Inject the view to be created into the script env.
                 scriptEnv->setNamespaces("UI", "View");
                 scriptEnv->addValue("port", view);
                 scriptEnv->setNamespaces();
@@ -224,6 +303,12 @@ namespace vorb {
 
                 return view;
             }
+        protected:
+            union {
+                vscript::ScriptEnvironmentBuilder<ScriptEnvironment>* m_scriptEnvironmentBuilder; ///< A builder callback for creating a script environment context.
+                vscript::EnvironmentRegistry<ScriptEnvironment>* m_scriptEnvironmentRegistry; ///< A registry from which to obtain script environments.
+            };
+            nString m_scriptEnvGroup; ///< The group to which to retrieve script environments from in the registry.
         };
 
         // TODO(Matthew): Handle situations where we want to provide further injections for certain UIs.

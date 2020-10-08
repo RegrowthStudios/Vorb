3d4260d1f3e3c4ed24ed86317a4c9f9c267fc15d
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2020-10-08 01:25:18 +0100

    Make sure UI, child of UIBase, also offers default font options.

--- include/Vorb/ui/UI.h
+++ include/Vorb/ui/UI.h
@@ -210,6 +210,7 @@ namespace vorb {
              * \param textureCache: The cache used for obtaining textures.
              * \param fontCache: The font used for rendering text in the UI.
              * \param spriteBatch: The batcher used for rendering other sprites of the UI.
+             * \param spriteFont: Optional default font to use for views created in this UI.
              */
             virtual void init(
                        IGameScreen* ownerScreen,
@@ -217,9 +218,10 @@ namespace vorb {
                 vio::IOManagerBase* ioManager,
                   vg::TextureCache* textureCache,
                      vg::FontCache* fontCache,
-                   vg::SpriteBatch* spriteBatch
+                   vg::SpriteBatch* spriteBatch,
+                    vg::SpriteFont* defaultFont = nullptr
             ) override {
-                init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch, nullptr, "");
+                init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch, nullptr, "", defaultFont);
             }
             /*!
              * \brief Initialises the UI with the necessary services to acquire files, textures, render fonts and othe spritesm etc.
@@ -231,6 +233,7 @@ namespace vorb {
              * \param fontCache: The font used for rendering text in the UI.
              * \param spriteBatch: The batcher used for rendering other sprites of the UI.
              * \param scriptEnvBuilder: The callback to use to build the context of an isolated script environment.
+             * \param spriteFont: Optional default font to use for views created in this UI.
              */
             void init(
                                                          IGameScreen* ownerScreen,
@@ -239,9 +242,10 @@ namespace vorb {
                                                     vg::TextureCache* textureCache,
                                                        vg::FontCache* fontCache,
                                                      vg::SpriteBatch* spriteBatch,
-                vscript::ScriptEnvironmentBuilder<ScriptEnvironment>* scriptEnvBuilder
+                vscript::ScriptEnvironmentBuilder<ScriptEnvironment>* scriptEnvBuilder,
+                                                      vg::SpriteFont* defaultFont = nullptr
             ) {
-                UIBase::init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch);
+                UIBase::init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch, defaultFont);
 
                 m_scriptEnvironmentBuilder = scriptEnvBuilder;
             }
@@ -256,6 +260,7 @@ namespace vorb {
              * \param spriteBatch: The batcher used for rendering other sprites of the UI.
              * \param scriptEnvRegistry: The registry from which to obtain script environments.
              * \param scriptEnvGroup: The group within the registry from which to obtain script environments.
+             * \param spriteFont: Optional default font to use for views created in this UI.
              */
             void init(
                                                     IGameScreen* ownerScreen,
@@ -265,9 +270,10 @@ namespace vorb {
                                                   vg::FontCache* fontCache,
                                                 vg::SpriteBatch* spriteBatch,
                 vscript::EnvironmentRegistry<ScriptEnvironment>* scriptEnvRegistry,
-                                                  const nString& scriptEnvGroup
+                                                  const nString& scriptEnvGroup,
+                                                 vg::SpriteFont* defaultFont = nullptr
             ) {
-                UIBase::init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch);
+                UIBase::init(ownerScreen, window, ioManager, textureCache, fontCache, spriteBatch, defaultFont);
 
                 m_scriptEnvironmentRegistry = scriptEnvRegistry;
                 m_scriptEnvGroup = scriptEnvGroup;
